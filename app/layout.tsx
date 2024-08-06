710298fcf55f695a53e5aa8266e3649ee1042ab0
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-06 11:37:25 +0200

    configured eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import { Providers } from "@/app/providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+
 import "@/app/globals.css";
+import { Providers } from "@/app/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
