0bbd50ec873d48cc79fc6614c9ed65d776f3d47e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-07 18:01:02 +0200

    feat(ds): added avatar group

---
+++ design-system/molecules/avatar-group/adapters/default/default.adapter.tsx
@@ -0,0 +1,59 @@
+import { ElementType } from "react";
+
+import { Avatar } from "@/design-system/atoms/avatar";
+
+import { cn } from "@/shared/helpers/cn";
+
+import { AvatarGroupPort } from "../../avatar-group.types";
+import { AvatarGroupDefaultVariants } from "./default.variants";
+
+export function AvatarGroupDefaultAdapter<C extends ElementType = "div">({
+  as,
+  htmlProps,
+  classNames,
+  avatars,
+  maxAvatars,
+  size,
+  shape,
+  container,
+  disableAnimation = true,
+  showFallback = true,
+}: AvatarGroupPort<C>) {
+  const Component = as || "div";
+  const slots = AvatarGroupDefaultVariants({
+    size,
+  });
+
+  const slicedAvatars = maxAvatars ? avatars.slice(0, maxAvatars) : avatars;
+
+  const avatarAnimation = {
+    base: "transition-transform hover:-translate-x-1/4 hover:last:translate-x-0",
+  };
+  return (
+    <Component {...htmlProps} className={cn(slots.base(), classNames?.base)}>
+      {slicedAvatars.map((avatar, index) => (
+        <Avatar
+          key={`avatar-${index}`}
+          src={avatar.src}
+          name={avatar.name}
+          size={size}
+          shape={shape}
+          container={container}
+          showFallback={showFallback}
+          classNames={!disableAnimation ? avatarAnimation : undefined}
+        />
+      ))}
+
+      {maxAvatars && avatars.length > maxAvatars ? (
+        <Avatar
+          name={`+${avatars.length - maxAvatars}`}
+          size={size}
+          shape={shape}
+          container={container}
+          showFallback={showFallback}
+          classNames={!disableAnimation ? avatarAnimation : undefined}
+        />
+      ) : null}
+    </Component>
+  );
+}
---
+++ design-system/molecules/avatar-group/avatar-group.types.ts
@@ -0,0 +1,20 @@
+import { ComponentPropsWithoutRef, ElementType } from "react";
+
+import { AvatarPort } from "@/design-system/atoms/avatar";
+
+interface Variants extends Pick<AvatarPort, "size" | "shape" | "container"> {}
+
+interface ClassNames {
+  base: string;
+}
+
+interface AvatarItem extends Pick<AvatarPort, "src" | "name"> {}
+
+export interface AvatarGroupPort<C extends ElementType> extends Partial<Variants>, Pick<AvatarPort, "showFallback"> {
+  as?: C;
+  htmlProps?: ComponentPropsWithoutRef<C>;
+  classNames?: Partial<ClassNames>;
+  avatars: AvatarItem[];
+  maxAvatars?: number;
+  disableAnimation?: boolean;
+}
