f4bedb06da9cb98ab813b9a54cda1e368f8cd653
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-07 17:47:34 +0200

    feat(ds): added tooltip

---
+++ design-system/atoms/tooltip/adapters/next-ui/next-ui.variants.ts
@@ -0,0 +1,9 @@
+import { tv } from "tailwind-variants";
+
+export const TooltipNextUiVariants = tv({
+  slots: {
+    wrapper: "",
+    tooltip:
+      "border-container-stroke-separator bg-container-action text-text-1 rounded-lg border p-2 text-xs shadow-none",
+  },
+});
