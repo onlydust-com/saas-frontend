38d5c0ca48e1c0408528991b83eb07809dba2c58
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-08 18:39:57 +0200

    WIP nav

--- shared/features/navigation/primary-menu/primary-menu.tsx
+++ shared/features/navigation/primary-menu/primary-menu.tsx
@@ -1,13 +1,11 @@
-import { ItemNav } from "@/design-system/molecules/item-nav";
-
 import { PrimaryMenuProps } from "./primary-menu.types";
 
 export function PrimaryMenu({ children }: PrimaryMenuProps) {
   return (
     <div className="w-full">
-      <ItemNav isFolded icon={{ name: "ri-line-chart-line" }} href={"/test"}>
-        Data
-      </ItemNav>
+      {/*<ItemNav isFolded icon={{ name: "ri-line-chart-line" }} href={"/test"}>*/}
+      {/*  Data*/}
+      {/*</ItemNav>*/}
     </div>
   );
 }
