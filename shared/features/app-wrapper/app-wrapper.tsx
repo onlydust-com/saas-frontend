e558776cb22be2102f5fed804b15d6b7b6e020ad
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-09 16:03:01 +0200

    Fix build

--- shared/features/app-wrapper/app-wrapper.tsx
+++ shared/features/app-wrapper/app-wrapper.tsx
@@ -2,7 +2,6 @@
 
 import { AnimatedColumnGroup } from "@/shared/components/animated-column-group/animated-column-group";
 import { AnimatedColumn } from "@/shared/components/animated-column-group/animated-column/animated-column";
-import { PrimaryNavigationMobile } from "@/shared/features/navigation/primary-navigation-mobile/primary-navigation-mobile";
 import { PrimaryNavigation } from "@/shared/features/navigation/primary-navigation/primary-navigation";
 import { useIsTablet } from "@/shared/hooks/ui/use-media-query";
 
@@ -13,8 +12,8 @@ export function AppWrapper({ children }: AppWrapperProps) {
 
   if (isTablet) {
     return (
-      <div className={"mx-auto h-dvh w-dvw max-w-[2560px] overflow-hidden p-3"}>
-        <PrimaryNavigationMobile />
+      <div className={"mx-auto h-dvh w-dvw overflow-hidden p-3"}>
+        <PrimaryNavigation />
         {children}
       </div>
     );
