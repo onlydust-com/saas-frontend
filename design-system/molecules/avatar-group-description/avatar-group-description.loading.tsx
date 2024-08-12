a121c2458dabe93db1beee0a0695237c82efd0a1
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-08-12 12:15:35 +0200

    Feat/ds avatar group description (#20)

--- design-system/molecules/avatar-group-description/avatar-group-description.loading.tsx
+++ design-system/molecules/avatar-group-description/avatar-group-description.loading.tsx
@@ -4,10 +4,11 @@ import { AvatarLoading } from "@/design-system/atoms/avatar/avatar.loading";
 import { Skeleton } from "@/design-system/atoms/skeleton";
 import { AvatarDescriptionDefaultVariants } from "@/design-system/molecules/avatar-description/adapters/default/default.variants";
 import { AvatarDescriptionPort } from "@/design-system/molecules/avatar-description/avatar-description.types";
+import { AvatarGroupLoading } from "@/design-system/molecules/avatar-group";
 
 import { cn } from "@/shared/helpers/cn";
 
-export function AvatarDescriptionLoading({
+export function AvatarGroupDescriptionLoading({
   avatarShape,
   classNames,
 }: {
@@ -18,7 +19,7 @@ export function AvatarDescriptionLoading({
 
   return (
     <div className={cn(slots.base(), classNames?.base)}>
-      <AvatarLoading size={"m"} shape={avatarShape} />
+      <AvatarGroupLoading size={"m"} shape={avatarShape} />
       <div className={"grid place-content-center content-between"}>
         <Skeleton classNames={{ base: "h-3 w-20" }} />
         <Skeleton classNames={{ base: "h-3 w-10" }} />
---
+++ design-system/molecules/avatar-group-description/index.ts
@@ -0,0 +1,3 @@
+export * from "./variants/avatar-group-description-default";
+export * from "./avatar-group-description.types";
+export * from "./avatar-group-description.loading";
