61e5c15f53898caac909a6d3de6759b15bdcaac2
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-12 11:07:06 +0200

    add date mock adapter

--- core/bootstrap/bootstrap-constructor-mock.ts
+++ core/bootstrap/bootstrap-constructor-mock.ts
@@ -1,10 +1,12 @@
 import { BootstrapConstructor } from "@/core/bootstrap/index";
 import { BannerClientAdapterMock } from "@/core/infrastructure/marketplace-api-client-adapter/mock-adapters/banner-client-adapter-mock";
 import { UserClientAdapterMock } from "@/core/infrastructure/marketplace-api-client-adapter/mock-adapters/user-client-adapter-mock";
+import { DateAdapterMock } from "@/core/kernel/date/date-adapter-mock";
 
 export const bootstrapConstructorMock: BootstrapConstructor = {
   userStoragePortForClient: new UserClientAdapterMock(),
   userStoragePortForServer: new UserClientAdapterMock(),
   bannerStoragePortForClient: new BannerClientAdapterMock(),
   bannerStoragePortForServer: new BannerClientAdapterMock(),
+  dateHelperPort: DateAdapterMock,
 };
