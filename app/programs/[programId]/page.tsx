1113f3cb2d21fb56c8ba397f6d5332dc19c5f6a2
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-12 14:35:59 +0200

    Add programs page

--- app/programs/[programId]/page.tsx
+++ app/programs/[programId]/page.tsx
@@ -1,7 +1,17 @@
-export default function ProgramPage() {
+"use client";
+
+import { ProgramReactQueryAdapter } from "@/core/application/react-query-adapter/program";
+
+export default function ProgramPage({ params: { programId } }: { params: { programId: string } }) {
+  const { data } = ProgramReactQueryAdapter.client.useGetProgramById({
+    pathParams: {
+      programId,
+    },
+  });
+
   return (
     <div>
-      <h1>Program Page</h1>
+      <h1>Program Page : {data?.name}</h1>
     </div>
   );
 }
--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { ProgramReactQueryAdapter } from "@/core/application/react-query-adapter/program";
+
+import { BaseLink } from "@/shared/components/base-link/base-link";
+import { NEXT_ROUTER } from "@/shared/constants/router";
+
 export default function ProgramsPage() {
+  const { data } = ProgramReactQueryAdapter.client.useGetPrograms({});
+  const programs = data?.pages.flatMap(page => page.programs) || [];
   return (
-    <div>
-      <h1>Programs Page</h1>
+    <div className="flex flex-col gap-3">
+      {programs.map(program => (
+        <div key={program.name}>
+          <BaseLink href={NEXT_ROUTER.programs.details.root(program.id)}>{program.name}</BaseLink>
+        </div>
+      ))}
     </div>
   );
 }
