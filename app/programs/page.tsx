d47e1b9e994bf05e86631a8359a704fa440ce25e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-13 15:41:50 +0200

    feat(programs): basic list

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import { createColumnHelper, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { useMemo } from "react";
+
 import { ProgramReactQueryAdapter } from "@/core/application/react-query-adapter/program";
+import { ProgramListItemInterface } from "@/core/domain/program/models/program-list-item-model";
+
+import { Button } from "@/design-system/atoms/button/variants/button-default";
+import { Typo } from "@/design-system/atoms/typo";
+import { Table } from "@/design-system/molecules/table";
 
 import { BaseLink } from "@/shared/components/base-link/base-link";
 import { NEXT_ROUTER } from "@/shared/constants/router";
@@ -10,7 +18,40 @@ import { Translate } from "@/shared/translation/components/translate/translate";
 
 export default function ProgramsPage() {
   const { data } = ProgramReactQueryAdapter.client.useGetPrograms({});
-  const programs = data?.pages.flatMap(page => page.programs) || [];
+  const programs = useMemo(() => data?.pages.flatMap(page => page.programs) ?? [], [data]);
+
+  const columnHelper = createColumnHelper<ProgramListItemInterface>();
+
+  const columns = [
+    columnHelper.accessor("id", {
+      header: () => <span>ID</span>,
+      cell: info => <Typo>{info.getValue()}</Typo>,
+    }),
+    columnHelper.accessor("name", {
+      header: () => <span>Name</span>,
+      cell: info => <Typo>{info.getValue()}</Typo>,
+    }),
+    columnHelper.display({
+      id: "actions",
+      header: () => <span>Actions</span>,
+      cell: info => (
+        <Button
+          as={BaseLink}
+          htmlProps={{ href: NEXT_ROUTER.programs.details.root(info.row.original.id) }}
+          variant={"secondary-light"}
+        >
+          See program
+        </Button>
+      ),
+    }),
+  ];
+
+  const table = useReactTable({
+    data: programs,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <PageWrapper
       navigation={{
@@ -18,18 +59,28 @@ export default function ProgramsPage() {
         breadcrumbs: [
           {
             id: "root",
-            label: <Translate token={"programs:details.header.title"} />,
+            label: <Translate token={"programs:list.header.title"} />,
           },
         ],
       }}
     >
       <PageContent>
-        <div className="flex flex-1 flex-col gap-3">
-          {programs.map(program => (
-            <div key={program.name}>
-              <BaseLink href={NEXT_ROUTER.programs.details.root(program.id)}>{program.name}</BaseLink>
-            </div>
-          ))}
+        <div className="grid gap-3">
+          <Typo
+            variant={"brand"}
+            size={"2xl"}
+            translate={{
+              token: "programs:list.content.title",
+            }}
+            color={"text-1"}
+          />
+
+          <Table
+            header={{
+              headerGroups: table.getHeaderGroups(),
+            }}
+            rows={table.getRowModel().rows}
+          />
         </div>
       </PageContent>
     </PageWrapper>
