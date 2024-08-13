4af490d992eb47b3a92708b5561f2ff4469482f2
Sami Lafrance <sami.lepays@gmail.com>
2024-08-13 09:54:31 +0200

    fix(ds): translate

--- design-system/molecules/cards/card-transaction/card-transaction.utils.tsx
+++ design-system/molecules/cards/card-transaction/card-transaction.utils.tsx
@@ -1,32 +1,36 @@
+import { ReactNode } from "react";
+
 import { IconPort } from "@/design-system/atoms/icon";
+
+import { Translate } from "@/shared/translation/components/translate/translate";
 
 import { CardTransactionStatus } from "./card-transaction.types";
 
 export function getComponentsVariants(status: CardTransactionStatus): {
   icon: IconPort;
-  statusName: string;
+  statusName: ReactNode;
 } {
-  const map: Record<CardTransactionStatus, { icon: IconPort; statusName: string }> = {
+  const map: Record<CardTransactionStatus, { icon: IconPort; statusName: ReactNode }> = {
     granted: {
       icon: {
         name: "ri-arrow-right-line",
         className: "text-label-blue",
       },
-      statusName: "Granted",
+      statusName: <Translate token="cards:cardTransaction.status.granted" />,
     },
     allocated: {
       icon: {
         name: "ri-arrow-down-line",
         className: "text-label-green",
       },
-      statusName: "Allocated",
+      statusName: <Translate token="cards:cardTransaction.status.allocated" />,
     },
     returned: {
       icon: {
         name: "ri-arrow-turn-forward-line",
         className: "text-label-red",
       },
-      statusName: "Returned",
+      statusName: <Translate token="cards:cardTransaction.status.returned" />,
     },
   };
 
