5a6a149c079692c9f5eed9a3dbb9c392e9ca2aa5
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-13 15:15:11 +0200

    feat(ds): table

---
+++ design-system/molecules/table/adapters/default/default.variants.ts
@@ -0,0 +1,9 @@
+import { tv } from "tailwind-variants";
+
+export const TableDefaultVariants = tv({
+  slots: {
+    base: "w-full border-separate border-spacing-y-3",
+  },
+  variants: {},
+  defaultVariants: {},
+});
---
+++ design-system/molecules/table/index.ts
@@ -0,0 +1,3 @@
+export * from "./variants/table-default";
+export * from "./table.types";
+export * from "./table.loading";
