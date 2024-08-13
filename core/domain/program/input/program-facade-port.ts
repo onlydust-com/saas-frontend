64ca7114d746d5c7d6fabdc16d7667e26aa0b3d3
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-13 16:12:03 +0200

    create Program Transaction query and model

--- core/domain/program/input/program-facade-port.ts
+++ core/domain/program/input/program-facade-port.ts
@@ -3,9 +3,12 @@ import {
   GetProgramByIdPortResponse,
   GetProgramsPortParams,
   GetProgramsPortResponse,
+  ProgramTransactionsStatsPortParams,
+  ProgramTransactionsStatsPortResponse,
 } from "../program-contract.types";
 
 export interface ProgramFacadePort {
   getPrograms(p: GetProgramsPortParams): GetProgramsPortResponse;
   getProgramById(p: GetProgramByIdPortParams): GetProgramByIdPortResponse;
+  getProgramTransactionsStats(p: ProgramTransactionsStatsPortParams): ProgramTransactionsStatsPortResponse;
 }
