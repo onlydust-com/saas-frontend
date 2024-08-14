d5d4666bd5679eba0597477f95430cdcc5543f0a
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-14 10:20:16 +0200

    feat(impersonation): banner

--- shared/features/app-wrapper/app-wrapper.tsx
+++ shared/features/app-wrapper/app-wrapper.tsx
@@ -2,16 +2,35 @@
 
 import { useAuth0 } from "@auth0/auth0-react";
 
+import { Typo } from "@/design-system/atoms/typo";
+
 import { AnimatedColumnGroup } from "@/shared/components/animated-column-group/animated-column-group";
 import { AnimatedColumn } from "@/shared/components/animated-column-group/animated-column/animated-column";
 import { PrimaryNavigation } from "@/shared/features/navigation/primary-navigation/primary-navigation";
 import { useIsTablet } from "@/shared/hooks/ui/use-media-query";
+import { useImpersonation } from "@/shared/providers/impersonation/impersonation-provider";
 
 import { AppWrapperProps } from "./app-wrapper.types";
 
+function ImpersonationBanner() {
+  const { isImpersonating } = useImpersonation();
+
+  if (!isImpersonating) return null;
+
+  return (
+    <div className={"pointer-events-none fixed bottom-4 left-0 right-0 z-10 text-center"}>
+      <div className={"relative inline-flex overflow-hidden rounded-xl bg-black p-3"}>
+        <div className="absolute inset-0 animate-pulse bg-label-red" />
+        <Typo weight={"medium"} classNames={{ base: "relative z-10" }}>
+          ⚠️ IMPERSONATING
+        </Typo>
+      </div>
+    </div>
+  );
+}
+
 export function AppWrapper({ children }: AppWrapperProps) {
   const isTablet = useIsTablet("lower");
-
   const { isAuthenticated, isLoading, error, loginWithRedirect } = useAuth0();
 
   // TODO add page skeleton
@@ -31,6 +50,7 @@ export function AppWrapper({ children }: AppWrapperProps) {
   if (isTablet) {
     return (
       <div className={"mx-auto flex h-dvh w-dvw flex-col gap-3 overflow-hidden p-3"}>
+        <ImpersonationBanner />
         <PrimaryNavigation />
         {children}
       </div>
@@ -39,6 +59,7 @@ export function AppWrapper({ children }: AppWrapperProps) {
 
   return (
     <div className={"mx-auto h-dvh w-dvw max-w-[2560px] overflow-hidden p-3"}>
+      <ImpersonationBanner />
       <AnimatedColumnGroup className="gap-3">
         <PrimaryNavigation />
         <AnimatedColumn autoWidth={true} className="size-full">
