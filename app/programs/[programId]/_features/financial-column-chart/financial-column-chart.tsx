708b48cb822972dad004d17f7326ed37722aed41
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-14 10:41:28 +0200

    WIP stats

--- app/programs/[programId]/_features/financial-column-chart/financial-column-chart.tsx
+++ app/programs/[programId]/_features/financial-column-chart/financial-column-chart.tsx
@@ -1,4 +1,9 @@
+import { useParams } from "next/navigation";
+
 import { ChartFooter } from "@/app/programs/[programId]/_features/financial-column-chart/components/chart-footer/chart-footer";
+
+import { ProgramReactQueryAdapter } from "@/core/application/react-query-adapter/program";
+import { bootstrap } from "@/core/bootstrap";
 
 import { Paper } from "@/design-system/atoms/paper";
 
@@ -6,187 +11,20 @@ import { ColumnChart } from "@/shared/components/charts/highcharts/column-chart/
 import { useColumnChartOptions } from "@/shared/components/charts/highcharts/column-chart/column-chart.hooks";
 
 export function FinancialColumnChart() {
-  const statsMock = [
-    {
-      date: "August 2024",
-      totalAvailable: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      totalGranted: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      totalRewarded: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      transactionCount: 42,
+  const { programId = "" } = useParams<{ programId: string }>();
+  const { data } = ProgramReactQueryAdapter.client.useGetProgramsTransactionsStats({
+    pathParams: {
+      programId,
     },
-    {
-      date: "September 2024",
-      totalAvailable: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      totalGranted: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      totalRewarded: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      transactionCount: 42,
-    },
-    {
-      date: "October 2024",
-      totalAvailable: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      totalGranted: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-      totalRewarded: {
-        totalUsdEquivalent: 100,
-        totalPerCurrency: [
-          {
-            amount: 100,
-            prettyAmount: 0,
-            currency: {
-              id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              code: "USDC",
-              name: "USD Coin",
-              logoUrl: "string",
-              decimals: 0,
-            },
-            usdEquivalent: 100,
-            usdConversionRate: 1.5,
-          },
-        ],
-      },
-    },
-  ];
+  });
 
-  const categories = statsMock.map(stat => stat.date);
+  const { stats } = data ?? {};
+  const { format } = bootstrap.getDateKernelPort();
 
-  const receivedSeries = statsMock.map(stat => stat.totalAvailable.totalUsdEquivalent);
-  const grantedSeries = statsMock.map(stat => stat.totalGranted.totalUsdEquivalent);
-  const rewardedSeries = statsMock.map(stat => stat.totalRewarded.totalUsdEquivalent);
+  const categories = stats?.map(stat => format(new Date(stat.date), "MMMM yyyy")) ?? [];
+  const receivedSeries = stats?.map(stat => stat.totalAvailable.totalUsdEquivalent) ?? [];
+  const grantedSeries = stats?.map(stat => stat.totalGranted.totalUsdEquivalent) ?? [];
+  const rewardedSeries = stats?.map(stat => stat.totalRewarded.totalUsdEquivalent) ?? [];
 
   const { options } = useColumnChartOptions({
     categories,
--- core/application/react-query-adapter/program/client/index.ts
+++ core/application/react-query-adapter/program/client/index.ts
@@ -1,2 +1,3 @@
 export * from "./use-get-program-by-id";
 export * from "./use-get-programs";
+export * from "./use-get-program-transactions-stats";
