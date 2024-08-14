0ceebbf32e0a67e14edeb4fb93f135c0753a8b77
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-08-14 15:13:13 +0200

    Refactor/routing behaviour (#43)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,36 @@
+import { Button } from "@/design-system/atoms/button/variants/button-default";
+import { Typo } from "@/design-system/atoms/typo";
+
+import { BaseLink } from "@/shared/components/base-link/base-link";
+import { NEXT_ROUTER } from "@/shared/constants/router";
+import { Translate } from "@/shared/translation/components/translate/translate";
+
 export default function NotFound() {
-  return <div>Oops, not found.</div>;
+  return (
+    <div className={"flex size-full items-center justify-center"}>
+      <div className={"flex flex-col items-center justify-center gap-6"}>
+        <Typo
+          variant={"brand"}
+          size={"4xl"}
+          classNames={{ base: "capitalize" }}
+          translate={{
+            token: "notFound:title",
+          }}
+        />
+
+        <Button
+          as={BaseLink}
+          htmlProps={{
+            href: NEXT_ROUTER.home.root,
+          }}
+          size={"l"}
+          startIcon={{
+            name: "ri-arrow-left-s-line",
+          }}
+        >
+          <Translate token={"notFound:back"} />
+        </Button>
+      </div>
+    </div>
+  );
 }
