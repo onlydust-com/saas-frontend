1600502b617167be1dad5ddb91a8f4e72a067355
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-08-19 18:18:12 +0200

    fix prop type

--- app/programs/[programId]/_features/budget-available-cards/budget-available-cards.types.ts
+++ app/programs/[programId]/_features/budget-available-cards/budget-available-cards.types.ts
@@ -1,11 +1,13 @@
 import { ProgramResponse } from "@/core/domain/program/models/program-model";
 
+import { TranslateProps } from "@/shared/translation/components/translate/translate.types";
+
 export interface CreateAvatarGroupProps {
   total: ProgramResponse["totalAvailable" | "totalGranted" | "totalRewarded"];
 }
 
 export interface FinancialCardItemProps {
-  title: string;
+  title: TranslateProps["token"];
   total: ProgramResponse["totalAvailable" | "totalGranted" | "totalRewarded"];
   color: "chart-1" | "chart-2" | "chart-3" | "chart-4";
 }
