712e740a04b96aea347901351d6b50c68512f67c
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-08-19 17:37:21 +0200

    reafactor(ds): table cell kpi trend

--- design-system/atoms/table-cell-kpi/adapters/default/default.adapter.tsx
+++ design-system/atoms/table-cell-kpi/adapters/default/default.adapter.tsx
@@ -6,13 +6,13 @@ import { cn } from "@/shared/helpers/cn";
 
 import { TableCellKpiDefaultVariants } from "./default.variants";
 
-export function TableCellKpiDefaultAdapter({ classNames, state, children }: TableCellKpiPort) {
-  const slots = TableCellKpiDefaultVariants({ state });
+export function TableCellKpiDefaultAdapter({ classNames, trend, inverted, children }: TableCellKpiPort) {
+  const slots = TableCellKpiDefaultVariants({ trend, inverted });
 
-  const iconNames: Record<NonNullable<typeof state>, IconPort["name"]> = {
-    positive: "ri-arrow-up-line",
-    negative: "ri-arrow-down-line",
-    neutral: "ri-arrow-right-line",
+  const iconNames: Record<NonNullable<typeof trend>, IconPort["name"]> = {
+    UP: "ri-arrow-up-line",
+    DOWN: "ri-arrow-down-line",
+    STABLE: "ri-arrow-right-line",
   };
 
   return (
@@ -20,7 +20,7 @@ export function TableCellKpiDefaultAdapter({ classNames, state, children }: Tabl
       <Typo size={"s"} weight={"medium"} color={"text-2"}>
         {children}
       </Typo>
-      {state ? <Icon name={iconNames[state]} className={cn(slots.icon())} /> : null}
+      {trend ? <Icon name={iconNames[trend]} className={cn(slots.icon())} /> : null}
     </div>
   );
 }
