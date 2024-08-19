3dd605720cb9c838f45e36eceec070064066bb31
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-08-19 15:07:24 +0200

    Program list table columns (#45)

--- core/domain/program/models/program-list-item-model.ts
+++ core/domain/program/models/program-list-item-model.ts
@@ -1,12 +1,17 @@
 import { components } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
 
-export type ProgramListItemResponse = components["schemas"]["ProgramShortResponse"];
+export type ProgramListItemResponse = components["schemas"]["ProgramPageItemResponse"];
 
 export interface ProgramListItemInterface extends ProgramListItemResponse {}
 
 export class ProgramListItem implements ProgramListItemInterface {
   id!: ProgramListItemResponse["id"];
+  leads!: ProgramListItemResponse["leads"];
   name!: ProgramListItemResponse["name"];
+  projectCount!: ProgramListItemResponse["projectCount"];
+  totalAvailable!: ProgramListItemResponse["totalAvailable"];
+  totalGranted!: ProgramListItemResponse["totalGranted"];
+  totalRewarded!: ProgramListItemResponse["totalRewarded"];
 
   constructor(props: ProgramListItemResponse) {
     Object.assign(this, props);
