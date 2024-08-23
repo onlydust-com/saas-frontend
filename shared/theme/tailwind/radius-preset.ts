3e130c72828960bfa1bc1282f33b6403f1a2ee24
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-23 17:36:26 +0200

    Update radius-preset.ts

--- shared/theme/tailwind/radius-preset.ts
+++ shared/theme/tailwind/radius-preset.ts
@@ -1,24 +1,22 @@
 import { Config } from "tailwindcss";
 
-export const RadiusSet = {
-  none: "var(--radius-none)",
-  xxs: "var(--radius-xxs)",
-  xs: "var(--radius-xs)",
-  sm: "var(--radius-sm)",
-  md: "var(--radius-md)",
-  lg: "var(--radius-lg)",
-  xl: "var(--radius-xl)",
-  "2xl": "var(--radius-2xl)",
-  "3xl": "var(--radius-3xl)",
-  "4xl": "var(--radius-4xl)",
-  full: "var(--radius-full)",
-};
-
 const RadiusPreset: Config = {
   content: [],
   theme: {
     extend: {
-      borderRadius: RadiusSet,
+      borderRadius: {
+        none: "var(--radius-none)",
+        xxs: "var(--radius-xxs)",
+        xs: "var(--radius-xs)",
+        sm: "var(--radius-sm)",
+        md: "var(--radius-md)",
+        lg: "var(--radius-lg)",
+        xl: "var(--radius-xl)",
+        "2xl": "var(--radius-2xl)",
+        "3xl": "var(--radius-3xl)",
+        "4xl": "var(--radius-4xl)",
+        full: "var(--radius-full)",
+      },
     },
   },
 };
