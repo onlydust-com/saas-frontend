a460e3985efe7e990fd7a8abb78a307d4c28e6af
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-08-23 15:42:12 +0200

    Add effect and gradients (#96)

---
+++ shared/theme/gradients.ts
@@ -0,0 +1,9 @@
+export const GRADIENTS = {
+  horizontal_1: "gradient-1-horizontal",
+  vertical_1: "gradient-1-vertical",
+  angular_1: "gradient-1-angular",
+  horizontal_2: "gradient-2-horizontal",
+  horizontal_3: "gradient-3-horizontal",
+  angular_3: "gradient-3-angular",
+  component_card_bg_gradient_1: "component-card-bg-gradient-1",
+} as const;
