3c3eeba507fb152a3373a652f7b08b19786e717f
Sami Lafrance <sami.lepays@gmail.com>
2024-08-23 11:34:41 +0200

    feat(*): WIP

--- design-system/atoms/avatar/avatar.types.ts
+++ design-system/atoms/avatar/avatar.types.ts
@@ -1,9 +1,8 @@
 import { ReactNode } from "react";
 
 interface Variants {
-  size: "xs" | "s" | "m" | "ml" | "l" | "xl" | "xxl";
-  shape: "round" | "square";
-  container: "light" | "brand";
+  size: "xxs" | "xs" | "s" | "md" | "lg" | "xl" | "2xl" | "3xl";
+  shape: "rounded" | "squared";
 }
 
 interface ClassNames {
@@ -14,30 +13,10 @@ interface ClassNames {
 }
 
 export interface AvatarPort extends Partial<Variants> {
-  /**
-   * Classname to change the classNames of the element. if className is passed, it will be added to the base slot.
-   * @default undefined
-   */
   classNames?: Partial<ClassNames>;
-  /**
-   * the source of the image
-   * @default undefined
-   */
   src?: string;
   alt?: string;
-  /**
-   * The name of the person in the avatar. - if src has loaded, the name will be used as the alt attribute of the img
-   * @default undefined
-   */
   name?: string;
-  /**
-   * If false, the avatar will show the background color while loading.
-   * @default true
-   */
   showFallback?: boolean;
-  /**
-   * Custom fallback component.
-   * @default undefined
-   */
   fallback?: ReactNode;
 }
