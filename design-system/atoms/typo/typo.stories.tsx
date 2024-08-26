af53b57d925ed6e104e8f38f8491f00fd1e1bfa3
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-26 16:19:09 +0200

    Update typo.stories.tsx

--- design-system/atoms/typo/typo.stories.tsx
+++ design-system/atoms/typo/typo.stories.tsx
@@ -13,7 +13,7 @@ const sizes = ["xs", "sm", "md", "lg", "xl", "2xl"] as const;
 
 const meta: Meta<typeof Typo> = {
   component: Typo,
-  title: "Deprecated/Atoms/Typo",
+  title: "Atoms/Typo",
   tags: ["autodocs"],
   parameters: {
     backgrounds: {
