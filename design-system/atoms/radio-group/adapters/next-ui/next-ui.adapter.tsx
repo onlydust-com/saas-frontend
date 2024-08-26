29a3d849ee62370fe536d0b9afa5c49adffe444e
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-26 19:00:39 +0200

    Radio group component

--- design-system/atoms/radio-group/adapters/next-ui/next-ui.adapter.tsx
+++ design-system/atoms/radio-group/adapters/next-ui/next-ui.adapter.tsx
@@ -1,9 +1,8 @@
 import { RadioGroup as NextRadioGroup } from "@nextui-org/radio";
 import { VisuallyHidden, useRadio } from "@nextui-org/react";
-import { Check } from "lucide-react";
-import { ElementType } from "react";
+import { ElementType, useState } from "react";
 
-import { Icon } from "@/design-system/atoms/icon";
+import { Typo } from "@/design-system/atoms/typo";
 
 import { cn } from "@/shared/helpers/cn";
 
@@ -13,37 +12,80 @@ import { RadioGroupNextUiVariants } from "./next-ui.variants";
 function Radio<V extends string, C extends ElementType = "div">({
   as,
   value,
-  mixed,
-  color = "white",
   classNames,
   componentProps,
+  attr = {},
+  title,
+  isDisabled: isDisabledProp,
+  description,
 }: RadioPort<V, C>) {
   const InnerComponent = as || "div";
-  const { Component, isSelected, getBaseProps, getInputProps, isDisabled } = useRadio({ value });
-  const slots = RadioGroupNextUiVariants({ isDisabled, isActive: isSelected, mixed, color });
-
+  const { Component, isSelected, getBaseProps, getInputProps, isDisabled: isDisabledGroup } = useRadio({ value });
+  const isDisabled = isDisabledProp || isDisabledGroup;
+  const slots = RadioGroupNextUiVariants({ isDisabled, isActive: isSelected });
+  const [isFocus, setIsFocus] = useState(false);
   return (
     <Component
       {...getBaseProps()}
+      data-focus={isFocus}
       className={cn(slots.item(), classNames?.item, { "pointer-events-none": isDisabled })}
+      {...attr}
     >
       <InnerComponent {...componentProps}>
         <VisuallyHidden>
-          <input {...getInputProps()} />
+          <input {...getInputProps()} onFocus={() => setIsFocus(true)} onBlur={() => setIsFocus(false)} />
         </VisuallyHidden>
-        <div className={cn(slots.indicator(), classNames?.indicator)}>
-          <Icon
-            component={Check}
-            classNames={{
-              base: cn(
+        <div className={"flex flex-row gap-md"}>
+          <div className={cn(slots.indicator(), classNames?.indicator)}>
+            <div
+              className={cn(
                 slots.indicatorIcon(),
                 {
-                  "opacity-100": isSelected && !mixed,
+                  "opacity-100": isSelected,
                 },
                 classNames?.indicatorIcon
-              ),
-            }}
-          />
+              )}
+            />
+          </div>
+          <div className={"flex flex-col"}>
+            {!!title && (
+              <Typo
+                variant={"text"}
+                size={"sm"}
+                weight={"medium"}
+                color={"secondary"}
+                canHover={true}
+                {...title}
+                classNames={{
+                  ...(title.classNames || {}),
+                  base: cn(
+                    {
+                      "text-typography-disabled": isDisabled,
+                    },
+                    title.classNames?.base
+                  ),
+                }}
+              />
+            )}
+            {!!description && (
+              <Typo
+                variant={"text"}
+                size={"sm"}
+                color={"secondary"}
+                canHover={true}
+                {...description}
+                classNames={{
+                  ...(description.classNames || {}),
+                  base: cn(
+                    {
+                      "text-typography-disabled": isDisabled,
+                    },
+                    description.classNames?.base
+                  ),
+                }}
+              />
+            )}
+          </div>
         </div>
       </InnerComponent>
     </Component>
@@ -56,10 +98,11 @@ export function RadioGroupNextUiAdapter<V extends string, C extends ElementType 
   onChange,
   items,
   value,
+  layout,
   ...props
 }: RadioGroupPort<V, C>) {
   const Component = as || "div";
-  const slots = RadioGroupNextUiVariants();
+  const slots = RadioGroupNextUiVariants({ layout });
 
   const handleChange = (value: string) => {
     onChange?.(value as V);
