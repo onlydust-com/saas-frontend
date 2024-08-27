b588ac7b32c69d0c5ecd84252900199cd352075b
Sami Lafrance <sami.lepays@gmail.com>
2024-08-27 17:08:04 +0200

    feat(*): Stepper

--- design-system/molecules/stepper/adapters/default/default.adapter.tsx
+++ design-system/molecules/stepper/adapters/default/default.adapter.tsx
@@ -5,13 +5,13 @@ import { cn } from "@/shared/helpers/cn";
 import { StepperPort } from "../../stepper.types";
 import { StepperDefaultVariants } from "./default.variants";
 
-export function StepperDefaultAdapter({ classNames, htmlProps, steps, color }: StepperPort) {
+export function StepperDefaultAdapter({ classNames, htmlProps, steps }: StepperPort) {
   const slots = StepperDefaultVariants();
 
   return (
     <div {...htmlProps} className={cn(slots.base(), classNames?.base)}>
       {steps.map((step, index) => (
-        <ProgressBar key={`step-${index}`} min={step.min} max={step.max} value={step.value} color={color} />
+        <ProgressBar key={`step-${index}`} value={step.value} />
       ))}
     </div>
   );
