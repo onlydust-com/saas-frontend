a133dd77610167eb50825a02ba1b0f0bbb149cd6
Alexis Benoliel <alexbenoliel@gmail.com>
2024-08-27 15:54:50 +0200

    Update paper

--- design-system/atoms/paper/paper.types.ts
+++ design-system/atoms/paper/paper.types.ts
@@ -1,9 +1,36 @@
 import { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from "react";
 
+import { COLORS } from "@/shared/theme/colors";
+
+type paperSpacing =
+  | "none"
+  | "xxs"
+  | "xs"
+  | "sm"
+  | "md"
+  | "2md"
+  | "lg"
+  | "2lg"
+  | "xl"
+  | "2xl"
+  | "3xl"
+  | "4xl"
+  | "5xl"
+  | "6xl"
+  | "7xl"
+  | "8xl"
+  | "9xl"
+  | "10xl"
+  | "11xl";
+
 interface Variants {
-  size: "s" | "m" | "l";
-  container: "1" | "2" | "3" | "4" | "action" | "inverse" | "transparent" | "interactions-black";
-  border: "none" | "container-stroke-separator";
+  size?: paperSpacing;
+  px?: paperSpacing;
+  py?: paperSpacing;
+  background: "transparent" | keyof typeof COLORS.background;
+  border: "none" | keyof typeof COLORS.border;
+  clickable?: boolean;
+  hasBorderHover?: boolean;
 }
 
 interface ClassNames {
