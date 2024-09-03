ff5fd0de1bd0ee8cf5d586239a8a02ae65f9a17f
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-09-03 10:32:46 +0200

    wip(project): sidepanel body scroll

--- shared/features/side-panels/side-panel-body/side-panel-body.tsx
+++ shared/features/side-panels/side-panel-body/side-panel-body.tsx
@@ -6,10 +6,8 @@ import { ScrollView } from "@/shared/components/scroll-view/scroll-view";
 
 export function SidePanelBody({ children }: PropsWithChildren) {
   return (
-    <ScrollView className={"h-full flex-1"}>
-      <Paper as={"div"} size={"lg"} classNames={{ base: "flex w-full flex-col gap-3 rounded-none h-full" }}>
-        {children}
-      </Paper>
-    </ScrollView>
+    <Paper as={ScrollView} size={"lg"} classNames={{ base: "flex flex-1 w-full flex-col gap-3 rounded-none h-full" }}>
+      {children}
+    </Paper>
   );
 }
