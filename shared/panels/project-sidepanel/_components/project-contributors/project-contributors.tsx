9b993c1bfd57f4f6c44ed2f0b7c45ea25da783d0
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-09-03 11:55:46 +0200

    refactor(project): contributors -> top contributors

--- shared/panels/project-sidepanel/_components/project-contributors/project-contributors.tsx
+++ shared/panels/project-sidepanel/_components/project-contributors/project-contributors.tsx
@@ -4,13 +4,18 @@ import { UserGroup } from "@/shared/features/user/user-group/user-group";
 
 import { ProjectContributorsProps } from "./project-contributors.types";
 
-export function ProjectContributors({ topContributors, contributorCount }: ProjectContributorsProps) {
+export function ProjectContributors({ topContributors }: ProjectContributorsProps) {
   if (!topContributors.length) return null;
 
   return (
     <div className={"flex flex-col gap-1"}>
-      <Typo size={"xs"} color={"secondary"} translate={{ token: "panels:projectDetail.contributors.title" }} />
-      <UserGroup avatarProps={{ size: "s" }} users={topContributors} totalUsersCount={contributorCount} maxUsers={2} />
+      <Typo size={"xs"} color={"secondary"} translate={{ token: "panels:projectDetail.topContributors.title" }} />
+      <UserGroup
+        avatarProps={{ size: "s" }}
+        users={topContributors}
+        totalUsersCount={topContributors.length}
+        maxUsers={2}
+      />
     </div>
   );
 }
--- shared/panels/project-sidepanel/_components/project-contributors/project-contributors.types.ts
+++ shared/panels/project-sidepanel/_components/project-contributors/project-contributors.types.ts
@@ -2,5 +2,4 @@ import { ProjectInterface } from "@/core/domain/project/models/project-model";
 
 export interface ProjectContributorsProps {
   topContributors: ProjectInterface["topContributors"];
-  contributorCount: ProjectInterface["contributorCount"];
 }
