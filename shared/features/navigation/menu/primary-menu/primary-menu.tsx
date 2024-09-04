deb30515712e98208fae2123e2d65142f400cd06
Alexis Benoliel <alexbenoliel@gmail.com>
2024-09-04 11:55:33 +0200

    Add program guard

--- shared/features/navigation/menu/primary-menu/primary-menu.tsx
+++ shared/features/navigation/menu/primary-menu/primary-menu.tsx
@@ -3,12 +3,14 @@ import { ChartLine, Clipboard, Compass, Wallet } from "lucide-react";
 import { ItemNav } from "@/design-system/molecules/item-nav";
 
 import { NEXT_ROUTER } from "@/shared/constants/router";
+import { useShowProgramsList } from "@/shared/hooks/programs/use-show-programs-list";
 import { useShowSponsorList } from "@/shared/hooks/sponsors/use-show-sponsor-list";
 
 import { PrimaryMenuProps } from "./primary-menu.types";
 
 export function PrimaryMenu({ isFolded }: PrimaryMenuProps) {
   const [showSponsorList] = useShowSponsorList();
+  const [showProgramList] = useShowProgramsList();
   return (
     <>
       <ItemNav
@@ -31,8 +33,13 @@ export function PrimaryMenu({ isFolded }: PrimaryMenuProps) {
       <ItemNav
         isFolded={isFolded}
         iconProps={{ component: Clipboard }}
-        linkProps={{ href: NEXT_ROUTER.programs.root }}
+        linkProps={{
+          href: showProgramList.hasMultiplePrograms
+            ? NEXT_ROUTER.programs.root
+            : NEXT_ROUTER.programs.details.root(showProgramList.firstProgram ?? ""),
+        }}
         translate={{ token: "primaryNavigation:primaryMenu.program" }}
+        isDisabled={showProgramList.loading}
       />
       <ItemNav
         isFolded={isFolded}
