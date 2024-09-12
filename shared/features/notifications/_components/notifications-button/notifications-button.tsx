2c152e3188d9f214fc65f18e40d0a5deba80a594
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-09-12 21:35:41 +0200

    feat(notifications): unread icon

--- shared/features/notifications/_components/notifications-button/notifications-button.tsx
+++ shared/features/notifications/_components/notifications-button/notifications-button.tsx
@@ -1,22 +1,29 @@
 import { Bell } from "lucide-react";
 
+import { NotificationReactQueryAdapter } from "@/core/application/react-query-adapter/notification";
+import { NotificationStatus } from "@/core/domain/notification/notification-constants";
+
 import { Button } from "@/design-system/atoms/button/variants/button-default";
 
 import { NotificationsButtonProps } from "@/shared/features/notifications/_components/notifications-button/notifications-button.types";
 
 export function NotificationsButton({ onClick }: NotificationsButtonProps) {
-  // TODO @hayden handle notification count request here
+  const { data: unreadNotificationCount } = NotificationReactQueryAdapter.client.useGetNotificationsCount({
+    queryParams: { status: NotificationStatus.UNREAD },
+  });
+
+  const hasUnreadNotifications = !!unreadNotificationCount?.count;
 
   return (
     <div className={"relative"}>
       <Button variant={"tertiary"} size={"xs"} startIcon={{ component: Bell }} iconOnly onClick={onClick} />
-      {/*{hasUnreadNotifications ? (*/}
-      {/*  <div*/}
-      {/*    className={*/}
-      {/*      "absolute right-0 top-0 size-1.5 -translate-y-1/4 translate-x-1/4 rounded-full bg-background-brand-primary"*/}
-      {/*    }*/}
-      {/*  />*/}
-      {/*) : null}*/}
+      {hasUnreadNotifications ? (
+        <div
+          className={
+            "absolute right-0 top-0 size-1.5 -translate-y-1/4 translate-x-1/4 rounded-full bg-background-brand-primary"
+          }
+        />
+      ) : null}
     </div>
   );
 }
