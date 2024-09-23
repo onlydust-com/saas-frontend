3b313ed90b2bdffd957f317821b1489c03f5af4d
Alexis Benoliel <alexbenoliel@gmail.com>
2024-09-23 14:33:06 +0200

    Add sandbox page

---
+++ app/data/sandbox/page.tsx
@@ -0,0 +1,7 @@
+export default function SandboxPage() {
+  return (
+    <div className={"flex h-full w-full items-start justify-center"}>
+      <div className={"h-full w-[384px] bg-background-primary-alt"}></div>
+    </div>
+  );
+}
