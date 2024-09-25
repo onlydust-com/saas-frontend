d555dd381014b55d7ff388368aeee239b073f269
Alexis Benoliel <alexbenoliel@gmail.com>
2024-09-25 14:19:16 +0200

    Add contributors filters

--- app/data/deep-dive/_features/contributors-table/contributors-table.tsx
+++ app/data/deep-dive/_features/contributors-table/contributors-table.tsx
@@ -1,13 +1,19 @@
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { Filter } from "lucide-react";
 import { useMemo, useState } from "react";
 
 import { ExportCsv } from "@/app/data/deep-dive/_features/contributors-table/_components/export-csv/export-csv";
 import { FilterColumns } from "@/app/data/deep-dive/_features/contributors-table/_components/filter-columns/filter-columns";
 import { useFilterColumns } from "@/app/data/deep-dive/_features/contributors-table/_components/filter-columns/filter-columns.hooks";
+import { FilterData } from "@/app/data/deep-dive/_features/contributors-table/_components/filter-data/filter-data";
+import { FilterDataProvider } from "@/app/data/deep-dive/_features/contributors-table/_components/filter-data/filter-data.context";
+import { useContributorFilterDataSidePanel } from "@/app/data/deep-dive/_features/contributors-table/_components/filter-data/filter-data.hooks";
 
 import { BiReactQueryAdapter } from "@/core/application/react-query-adapter/bi";
-import { GetBiContributorsQueryParams } from "@/core/domain/bi/bi-contract.types";
+import { GetBiContributorsPortParams, GetBiContributorsQueryParams } from "@/core/domain/bi/bi-contract.types";
 
+import { Badge } from "@/design-system/atoms/badge";
+import { Button } from "@/design-system/atoms/button/variants/button-default";
 import { Table, TableLoading } from "@/design-system/molecules/table";
 import { TableSearch } from "@/design-system/molecules/table-search";
 
@@ -16,10 +22,16 @@ import { ScrollView } from "@/shared/components/scroll-view/scroll-view";
 import { ShowMore } from "@/shared/components/show-more/show-more";
 import { useAuthUser } from "@/shared/hooks/auth/use-auth-user";
 
+export type ContributorsTableFilters = Omit<
+  NonNullable<GetBiContributorsPortParams["queryParams"]>,
+  "pageSize" | "pageIndex"
+>;
+
 export function ContributorsTable() {
+  const { open: openFilterPanel } = useContributorFilterDataSidePanel();
   const [search, setSearch] = useState<string>();
   const [debouncedSearch, setDebouncedSearch] = useState<string>();
-
+  const [filters, setFilters] = useState<ContributorsTableFilters>({});
   const { user, isLoading: isLoadingUser, isError: isErrorUser } = useAuthUser();
   const userProgramIds = user?.programs?.map(program => program.id) ?? [];
   const userEcosystemIds = user?.ecosystems?.map(ecosystem => ecosystem.id) ?? [];
@@ -27,6 +39,7 @@ export function ContributorsTable() {
   const queryParams: Partial<GetBiContributorsQueryParams> = {
     programOrEcosystemIds: [...userProgramIds, ...userEcosystemIds],
     search: debouncedSearch,
+    ...filters,
   };
 
   const {
@@ -64,25 +77,41 @@ export function ContributorsTable() {
     return <ErrorState />;
   }
 
+  const filtersCount = Object.keys(filters)?.length;
+
   return (
-    <div className={"grid gap-lg"}>
-      <nav className={"flex gap-md"}>
-        <TableSearch value={search} onChange={setSearch} onDebouncedChange={setDebouncedSearch} />
-        <FilterColumns selectedIds={selectedIds} setSelectedIds={setSelectedIds} />
-        <ExportCsv queryParams={queryParams} />
-      </nav>
-      <ScrollView direction={"x"}>
-        <Table
-          header={{
-            headerGroups: table.getHeaderGroups(),
-          }}
-          rows={table.getRowModel().rows}
-          classNames={{
-            base: "min-w-[1200px]",
-          }}
-        />
-        {hasNextPage ? <ShowMore onNext={fetchNextPage} loading={isFetchingNextPage} /> : null}
-      </ScrollView>
-    </div>
+    <FilterDataProvider filters={filters} setFilters={setFilters}>
+      <div className={"grid gap-lg"}>
+        <nav className={"flex gap-md"}>
+          <Button
+            variant={"secondary"}
+            size="sm"
+            startIcon={{ component: Filter }}
+            iconOnly={!filtersCount}
+            onClick={() => openFilterPanel()}
+            classNames={{
+              content: "w-fit",
+            }}
+            endContent={filtersCount ? <Badge size={"xxs"}>{filtersCount}</Badge> : undefined}
+          />
+          <TableSearch value={search} onChange={setSearch} onDebouncedChange={setDebouncedSearch} />
+          <FilterColumns selectedIds={selectedIds} setSelectedIds={setSelectedIds} />
+          <ExportCsv queryParams={queryParams} />
+        </nav>
+        <ScrollView direction={"x"}>
+          <Table
+            header={{
+              headerGroups: table.getHeaderGroups(),
+            }}
+            rows={table.getRowModel().rows}
+            classNames={{
+              base: "min-w-[1200px]",
+            }}
+          />
+          {hasNextPage ? <ShowMore onNext={fetchNextPage} loading={isFetchingNextPage} /> : null}
+        </ScrollView>
+      </div>
+      <FilterData />
+    </FilterDataProvider>
   );
 }
