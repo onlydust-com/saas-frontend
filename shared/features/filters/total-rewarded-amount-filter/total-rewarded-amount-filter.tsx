dc13a1a7256fa31270d723c97d9445d197a46cc7
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-09-25 17:03:52 +0200

    add unit to total reward amount filter (#274)

--- shared/features/filters/total-rewarded-amount-filter/total-rewarded-amount-filter.tsx
+++ shared/features/filters/total-rewarded-amount-filter/total-rewarded-amount-filter.tsx
@@ -3,14 +3,14 @@ import { QuantityFilter } from "@/shared/features/filters/quantity-filter/quanti
 
 import { TotalRewardedAmountFilterProps } from "./total-rewarded-amount-filter.types";
 
-export function TotalRewardedAmountFilter({ onChange, value }: TotalRewardedAmountFilterProps) {
+export function TotalRewardedAmountFilter({ onChange, value, unit }: TotalRewardedAmountFilterProps) {
   return (
     <AccordionFilter
       name={"total-rewarded-amount"}
       title={{ translate: { token: "features:filters.totalRewardedAmount.title" } }}
       selected={value?.amount ? 1 : 0}
     >
-      <QuantityFilter name={"total-rewarded-amount"} value={value} onChange={onChange} />
+      <QuantityFilter name={"total-rewarded-amount"} value={value} onChange={onChange} unit={unit} />
     </AccordionFilter>
   );
 }
