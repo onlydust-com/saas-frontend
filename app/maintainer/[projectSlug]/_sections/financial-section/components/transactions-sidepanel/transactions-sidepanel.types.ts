5847f3d75d96b2f61ebe318ae04b766ab7e86176
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-09-27 11:11:16 +0200

    feat(maintainer): copied financial section

---
+++ app/maintainer/[projectSlug]/_sections/financial-section/components/transactions-sidepanel/transactions-sidepanel.types.ts
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react";
+
+export enum TransactionsSidepanels {
+  TRANSACTIONS = "transactions",
+  EXPORT = "export",
+}
+
+export interface TransactionsSidepanelProps extends PropsWithChildren {}
--- app/manage-projects/[projectSlug]/page.tsx
+++ app/manage-projects/[projectSlug]/page.tsx
@@ -4,6 +4,8 @@ import { withAuthenticationRequired } from "@auth0/auth0-react";
 
 import { Button } from "@/design-system/atoms/button/variants/button-default";
 
+import { FinancialSection } from "@/app/maintainer/[projectSlug]/_sections/financial-section/financial-section";
+
 import { AnimatedColumn } from "@/shared/components/animated-column-group/animated-column/animated-column";
 import { withClientOnly } from "@/shared/components/client-only/client-only";
 import { ScrollView } from "@/shared/components/scroll-view/scroll-view";
@@ -31,7 +33,8 @@ function UpdateProjectSandbox() {
   );
 }
 
-function MaintainerSinglePage() {
+function MaintainerSinglePage({ params: { projectSlug } }: { params: { projectSlug: string } }) {
+  // TODO handle request to get project details: get project name for breadcrumbs
   return (
     <PageWrapper
       navigation={{
@@ -51,7 +54,7 @@ function MaintainerSinglePage() {
       <AnimatedColumn className="flex h-full flex-1 flex-col gap-md overflow-auto">
         <ScrollView>
           <PageContent>
-            <div>content</div>
+            <FinancialSection projectSlug={projectSlug} />
             <UpdateProjectSandbox />
           </PageContent>
         </ScrollView>
