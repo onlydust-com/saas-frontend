3e3e024724c78cec30facfc4d7862dddaea5c752
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-09-27 11:11:16 +0200

    feat(maintainer): copied financial section

---
+++ app/maintainer/[projectSlug]/_sections/financial-section/components/budget-available-cards/budget-available-cards.types.ts
@@ -0,0 +1,17 @@
+import { SponsorInterface } from "@/core/domain/sponsor/models/sponsor-model";
+import { AnyType } from "@/core/kernel/types";
+
+import { CardFinancialPort } from "@/design-system/molecules/cards/card-financial/card-financial.types";
+
+import { TranslateProps } from "@/shared/translation/components/translate/translate.types";
+
+export interface CreateAvatarGroupProps {
+  total: SponsorInterface["totalAllocated" | "totalAvailable" | "totalGranted" | "totalRewarded"];
+}
+
+export interface FinancialCardItemProps {
+  title: TranslateProps["token"];
+  total: SponsorInterface["totalAllocated" | "totalAvailable" | "totalGranted" | "totalRewarded"];
+  color: CardFinancialPort<AnyType>["color"];
+  onClick?: () => void;
+}
