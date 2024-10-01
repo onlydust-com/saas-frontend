83eee2da9f3ebc16ea50f2d52e907304550eeef9
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-10-01 12:34:51 +0200

    refacotr label popover and autocomplete

--- shared/features/autocompletes/label-autocomplete/label-autocomplete.tsx
+++ shared/features/autocompletes/label-autocomplete/label-autocomplete.tsx
@@ -1,11 +1,30 @@
-import { MenuItemId } from "@/design-system/molecules/menu-item";
+import { useMemo } from "react";
+
+import { MenuItemId, MenuItemPort } from "@/design-system/molecules/menu-item";
 import { Select } from "@/design-system/molecules/select";
 
-import { useLabelAutocomplete } from "@/shared/features/autocompletes/category-autocomplete/label-autocomplete.hooks";
 import { LabelAutocompleteProps } from "@/shared/features/autocompletes/label-autocomplete/label-autocomplete.types";
 
 export function LabelAutocomplete({ selectedLabels, onSelect, ...selectProps }: LabelAutocompleteProps) {
-  const { labelsItem } = useLabelAutocomplete();
+  // TODO retrieve labels data with dedicated query hook
+  // Temp mock data
+  const data = {
+    labels: [
+      { id: "item1", name: "Item 1" },
+      { id: "item2", name: "Item 2" },
+      { id: "item3", name: "Item 3" },
+    ],
+  };
+
+  const labelsItem: MenuItemPort[] = useMemo(() => {
+    return (
+      data?.labels?.map(label => ({
+        id: label.id,
+        label: label.name,
+        searchValue: label.name,
+      })) ?? []
+    );
+  }, [data]);
 
   function handleSelect(ids: MenuItemId[]) {
     onSelect?.(ids as string[]);
