a5c30a252497a92653b988ec552a6e1132c72ed1
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-10-03 11:22:03 +0200

    styles(*): page spacing

--- app/manage-projects/[projectSlug]/page.tsx
+++ app/manage-projects/[projectSlug]/page.tsx
@@ -51,17 +51,13 @@ function ManageProjectsSinglePage({ params: { projectSlug } }: { params: { proje
         paramsReady={Boolean(data?.id)}
       />
 
-      <AnimatedColumn className="flex h-full flex-1 flex-col gap-md overflow-auto">
-        <ScrollView className="flex flex-col gap-4">
+      <AnimatedColumn className="h-full">
+        <ScrollView className="flex flex-col gap-md">
           <PageContent classNames={{ base: "flex-none" }}>
-            <div className="grid gap-3">
-              <FinancialSection projectId={data?.id} />
-            </div>
+            <FinancialSection projectId={data?.id} />
           </PageContent>
           <PageContent>
-            <div className="grid h-full gap-3">
-              <ContributorsTable />
-            </div>
+            <ContributorsTable />
           </PageContent>
         </ScrollView>
       </AnimatedColumn>
