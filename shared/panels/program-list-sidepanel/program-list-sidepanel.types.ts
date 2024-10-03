6eadb771c52d188dc488277928bb69d97f0be5cb
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-10-03 11:06:56 +0200

    fix(financial): create program from sidepanel

--- shared/panels/program-list-sidepanel/program-list-sidepanel.types.ts
+++ shared/panels/program-list-sidepanel/program-list-sidepanel.types.ts
@@ -3,11 +3,9 @@ import { PropsWithChildren } from "react";
 export interface ProgramListSidepanelProps {
   sponsorId: string;
   onProgramClick?: (programId: string) => void;
-  onCreateProgramClick: () => void;
 }
 
 export interface ProgramListSidepanelContextProps extends PropsWithChildren {
   sponsorId: string;
   onProgramClick?: (programId: string) => void;
-  onCreateProgramClick: () => void;
 }
