3cdf3cf248209ef0c695902f3b88b21b5e8412a1
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-10-04 15:13:31 +0200

    feat(core): contribution activity (#324)

---
+++ core/infrastructure/marketplace-api-client-adapter/mock-adapters/contribution-client-adapter-mock.ts
@@ -0,0 +1,10 @@
+import { ContributionStoragePort } from "@/core/domain/contribution/output/contribution-storage-port";
+import { mockHttpStorageResponse } from "@/core/infrastructure/marketplace-api-client-adapter/http/mock-http-client/mock-http-storage-response";
+
+export class ContributionClientAdapterMock implements ContributionStoragePort {
+  constructor() {}
+
+  routes = {};
+
+  getContributions = mockHttpStorageResponse<ContributionStoragePort["getContributions"]>;
+}
