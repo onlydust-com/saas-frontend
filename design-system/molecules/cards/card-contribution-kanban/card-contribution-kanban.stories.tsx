997ebbba76390bfdca1211232d43a2e5a9ccaf56
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-10-07 15:43:16 +0200

    fix(ds): contribution kanban card story

--- design-system/molecules/cards/card-contribution-kanban/card-contribution-kanban.stories.tsx
+++ design-system/molecules/cards/card-contribution-kanban/card-contribution-kanban.stories.tsx
@@ -1,95 +1,113 @@
 import { Meta, StoryObj } from "@storybook/react";
 
+import { ContributionActivity } from "@/core/domain/contribution/models/contribution-activity-model";
+
 import { CardContributionKanbanPort } from "@/design-system/molecules/cards/card-contribution-kanban/card-contribution-kanban.types";
 import { CardContributionKanban } from "@/design-system/molecules/cards/card-contribution-kanban/variants/card-contribution-kanban-default";
 
 type Story = StoryObj<typeof CardContributionKanban>;
 
-const defaultProps: CardContributionKanbanPort<"div"> = {
-  contribution: {
-    type: "ISSUE",
-    repo: {
-      id: 650626566,
-      owner: "onlydustxyz",
-      name: "marketplace-backend",
-      description: "Awesome repo",
-      htmlUrl: "https://github.com/onlydustxyz/marketplace-backend",
+const contribution = new ContributionActivity({
+  type: "ISSUE",
+  repo: {
+    id: 650626566,
+    owner: "onlydustxyz",
+    name: "marketplace-backend",
+    description: "Awesome repo",
+    htmlUrl: "https://github.com/onlydustxyz/marketplace-backend",
+  },
+  githubAuthor: {
+    githubUserId: 595505,
+    login: "ofux",
+    avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+  },
+  githubNumber: 6,
+  githubStatus: "OPEN",
+  githubTitle: "string",
+  githubHtmlUrl: "string",
+  githubBody: "string",
+  githubCodeReviewOutcome: "PENDING",
+  githubLabels: [
+    {
+      name: "string",
+      description: "string",
     },
-    githubAuthor: {
+  ],
+  id: "string",
+  createdAt: "2024-10-04T12:45:02.234Z",
+  completedAt: "2024-10-04T12:45:02.234Z",
+  lastUpdatedAt: "2024-10-04T12:45:02.234Z",
+  activityStatus: "NOT_ASSIGNED",
+  contributors: [
+    {
       githubUserId: 595505,
       login: "ofux",
       avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+      isRegistered: true,
+      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
     },
-    githubNumber: 6,
-    githubStatus: "OPEN",
-    githubTitle: "string",
-    githubHtmlUrl: "string",
-    githubBody: "string",
-    githubCodeReviewOutcome: "PENDING",
-    githubLabels: [
-      {
-        name: "string",
-        description: "string",
+    {
+      githubUserId: 595505,
+      login: "ofux",
+      avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+      isRegistered: true,
+      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    },
+  ],
+  applicants: [
+    {
+      githubUserId: 595505,
+      login: "ofux",
+      avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+      isRegistered: true,
+      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    },
+    {
+      githubUserId: 595505,
+      login: "ofux",
+      avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
+      isRegistered: true,
+      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    },
+  ],
+  linkedIssues: [
+    {
+      type: "ISSUE",
+      repo: {
+        id: 650626566,
+        owner: "onlydustxyz",
+        name: "marketplace-backend",
+        description: "Awesome repo",
+        htmlUrl: "https://github.com/onlydustxyz/marketplace-backend",
       },
-    ],
-    id: "string",
-    createdAt: "2024-10-04T12:45:02.234Z",
-    completedAt: "2024-10-04T12:45:02.234Z",
-    lastUpdatedAt: "2024-10-04T12:45:02.234Z",
-    activityStatus: "NOT_ASSIGNED",
-    contributors: [
-      {
+      githubAuthor: {
         githubUserId: 595505,
         login: "ofux",
         avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
-        isRegistered: true,
-        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
       },
-    ],
-    applicants: [
-      {
-        githubUserId: 595505,
-        login: "ofux",
-        avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
-        isRegistered: true,
-        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      },
-    ],
-    linkedIssues: [
-      {
-        type: "ISSUE",
-        repo: {
-          id: 650626566,
-          owner: "onlydustxyz",
-          name: "marketplace-backend",
-          description: "Awesome repo",
-          htmlUrl: "https://github.com/onlydustxyz/marketplace-backend",
+      githubNumber: 6,
+      githubStatus: "OPEN",
+      githubTitle: "string",
+      githubHtmlUrl: "string",
+      githubBody: "string",
+      githubCodeReviewOutcome: "PENDING",
+      githubLabels: [
+        {
+          name: "string",
+          description: "string",
         },
-        githubAuthor: {
-          githubUserId: 595505,
-          login: "ofux",
-          avatarUrl: "https://avatars.githubusercontent.com/u/595505?v=4",
-        },
-        githubNumber: 6,
-        githubStatus: "OPEN",
-        githubTitle: "string",
-        githubHtmlUrl: "string",
-        githubBody: "string",
-        githubCodeReviewOutcome: "PENDING",
-        githubLabels: [
-          {
-            name: "string",
-            description: "string",
-          },
-        ],
-      },
-    ],
-  },
+      ],
+    },
+  ],
+});
+
+const defaultProps: CardContributionKanbanPort<"div"> = {
+  contribution,
 };
 
 const meta: Meta<typeof CardContributionKanban> = {
   component: CardContributionKanban,
-  title: "Molecules/CardContributionKanban",
+  title: "Molecules/Cards/CardContributionKanban",
   tags: ["autodocs"],
 };
 
