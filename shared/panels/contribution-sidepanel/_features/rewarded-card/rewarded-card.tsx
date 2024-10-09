6f369d8a5ad1401f826d40a2d67dc82cab3a1db0
Alexis Benoliel <alexbenoliel@gmail.com>
2024-10-09 18:49:10 +0200

    Add gradient

--- shared/panels/contribution-sidepanel/_features/rewarded-card/rewarded-card.tsx
+++ shared/panels/contribution-sidepanel/_features/rewarded-card/rewarded-card.tsx
@@ -1,10 +1,8 @@
-import { ArrowRight, Clock } from "lucide-react";
+import { Clock } from "lucide-react";
 
 import { bootstrap } from "@/core/bootstrap";
 
-import { Avatar } from "@/design-system/atoms/avatar";
 import { Badge } from "@/design-system/atoms/badge";
-import { Icon } from "@/design-system/atoms/icon";
 import { Paper } from "@/design-system/atoms/paper";
 import { Typo } from "@/design-system/atoms/typo";
 
@@ -27,36 +25,27 @@ export function RewardedCard({ reward }: RewardedCardProps) {
   });
 
   return (
-    <Paper border="primary" size="lg" classNames={{ base: "flex flex-col gap-lg gradient-glass-neon-80" }}>
-      <Typo size="sm" weight="medium" translate={{ token: "panels:contribution.rewardedCard.title" }} />
-
-      <div className="flex gap-lg">
-        <Avatar src={reward.currency.logoUrl} size="sm" />
-
-        <div className="flex flex-col gap-md">
-          <div className="grid">
-            <div className="flex items-center gap-xs">
-              <Typo size="sm" weight="medium">
-                {titleMoney.amount} {titleMoney.code}
-              </Typo>
-
-              <Icon component={ArrowRight} classNames={{ base: "text-utility-secondary-blue-500" }} />
-            </div>
-
-            <Typo size="xs" color="secondary">
-              ~{descriptionMoney.amount} {descriptionMoney.code}
-            </Typo>
-          </div>
-
-          <div className="flex w-full flex-wrap gap-md">
-            <Badge color="brand" size="xs">
+    <Paper border="primary" size="lg" classNames={{ base: "flex flex-col gap-lg app_gradient overflow-hidden" }}>
+      <div className="relative z-[1] flex flex-col gap-3xl">
+        <div className="flex justify-between gap-lg">
+          <Typo size="sm" weight="medium" translate={{ token: "panels:contribution.rewardedCard.title" }} />
+          <div className="flex w-full flex-wrap justify-end gap-md">
+            <Badge color="grey" size="xs">
               <Translate token="panels:contribution.rewardedCard.status" />
             </Badge>
 
-            <Badge icon={{ component: Clock }} color="brand" size="xs">
+            <Badge icon={{ component: Clock }} color="grey" size="xs">
               {dateKernelPort.format(new Date(), "dd.MM.yyyy")}
             </Badge>
           </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <Typo size="md" variant={"heading"} weight="medium">
+            {titleMoney.amount} {titleMoney.code}
+          </Typo>
+          <Typo size="md" color="secondary">
+            ~{descriptionMoney.amount} {descriptionMoney.code}
+          </Typo>
         </div>
       </div>
     </Paper>
