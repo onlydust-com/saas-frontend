be50940ea0d2ae963c3680fc8ac665cea519890f
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-10-09 10:48:19 +0200

    e-2099-applications-table (#343)

---
+++ shared/modals/manage-applicants-modal/_components/applicants-table/_components/filter-columns/filter-columns.tsx
@@ -0,0 +1,73 @@
+import { TableColumnList } from "@/design-system/molecules/table-column-list";
+
+import { Translate } from "@/shared/translation/components/translate/translate";
+
+import { FilterColumnsProps, TableColumns } from "./filter-columns.types";
+
+export function FilterColumns({ selectedIds, setSelectedIds }: FilterColumnsProps) {
+  return (
+    <TableColumnList
+      titleProps={{ token: "modals:manageApplicants.table.filters.columnList" }}
+      menuProps={{
+        items: [
+          {
+            id: "global",
+            label: <Translate token={"modals:manageApplicants.table.filters.global"} />,
+            isLabel: true,
+          },
+          {
+            id: "contributor",
+            label: <Translate token={"modals:manageApplicants.table.columns.contributor"} />,
+            searchValue: "Contributor",
+            isCheckbox: true,
+          },
+          {
+            id: "label",
+            label: <Translate token={"modals:manageApplicants.table.columns.label"} />,
+            searchValue: "Projects",
+            isCheckbox: true,
+          },
+          {
+            id: "languages",
+            label: <Translate token={"modals:manageApplicants.table.columns.languages"} />,
+            searchValue: "Languages",
+            isCheckbox: true,
+          },
+          {
+            id: "ecosystems",
+            label: <Translate token={"modals:manageApplicants.table.columns.ecosystems"} />,
+            searchValue: "Ecosystems",
+            isCheckbox: true,
+          },
+          {
+            id: "country",
+            label: <Translate token={"modals:manageApplicants.table.columns.country"} />,
+            searchValue: "Country",
+            isCheckbox: true,
+          },
+          {
+            id: "globalSeparator",
+            isSeparator: true,
+          },
+          {
+            id: "financial",
+            label: <Translate token={"modals:manageApplicants.table.filters.financial"} />,
+            isLabel: true,
+          },
+          {
+            id: "rewardedAmount",
+            label: <Translate token={"modals:manageApplicants.table.columns.rewardedAmount"} />,
+            searchValue: "Rewarded amount",
+            isCheckbox: true,
+          },
+        ],
+        selectedIds,
+        onSelect: ids => setSelectedIds(ids as Array<TableColumns>),
+        isMultiple: true,
+      }}
+      popoverProps={{
+        placement: "bottom-end",
+      }}
+    />
+  );
+}
