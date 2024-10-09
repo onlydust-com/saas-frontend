f539812b11ee3abd163c0343cc01def681b9bee5
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-10-09 18:02:02 +0200

    handle footer github link

--- shared/panels/contribution-sidepanel/_features/footer/footer.tsx
+++ shared/panels/contribution-sidepanel/_features/footer/footer.tsx
@@ -1,7 +1,10 @@
+import { Github } from "lucide-react";
+
 import { ContributionReactQueryAdapter } from "@/core/application/react-query-adapter/contribution";
 
 import { Button } from "@/design-system/atoms/button/variants/button-default";
 
+import { BaseLink } from "@/shared/components/base-link/base-link";
 import { SidePanelFooter } from "@/shared/features/side-panels/side-panel-footer/side-panel-footer";
 import { ManageApplicantsModal } from "@/shared/modals/manage-applicants-modal/manage-applicants-modal";
 import { useManageApplicantsModal } from "@/shared/modals/manage-applicants-modal/manage-applicants-modal.hooks";
@@ -25,10 +28,17 @@ export function Footer({ contribution }: FooterProps) {
 
   return (
     <SidePanelFooter>
-      <div className={"flex w-full flex-row items-center justify-end gap-lg"}>
-        {/*<Button size={"md"} variant={"secondary"} onClick={() => resetFilters()}>*/}
-        {/*  <Translate token={"data:deepDive.filters.reset"} />*/}
-        {/*</Button>*/}
+      <div className={"flex w-full flex-row items-center justify-between gap-lg"}>
+        <Button
+          size={"md"}
+          variant={"secondary"}
+          as={BaseLink}
+          iconOnly
+          htmlProps={{ href: contribution?.githubHtmlUrl ?? "", target: "_blank" }}
+          startIcon={{
+            component: Github,
+          }}
+        />
 
         {contribution?.isNotAssigned() ? (
           <Button
