c1d843ae9fde68428dcf3c39fdd6ac4a973fe3f7
Sami Lafrance <sami.lepays@gmail.com>
2024-10-09 11:55:16 +0200

    Devs selection component (#342)

---
+++ shared/panels/contribution-sidepanel/_features/applications-accordion/applications-accordion.types.ts
@@ -0,0 +1,10 @@
+import { ApplicationListItemInterface } from "@/core/domain/application/models/application-list-item-model";
+
+export interface ApplicationsAccordionProps {
+  activeApplicants: ApplicationListItemInterface[];
+  activeApplicantsCount: number;
+  newApplicants: ApplicationListItemInterface[];
+  newApplicantsCount: number;
+  ignoredApplicants: ApplicationListItemInterface[];
+  ignoredApplicantsCount: number;
+}
--- shared/panels/contribution-sidepanel/contributions-sidepanel.tsx
+++ shared/panels/contribution-sidepanel/contributions-sidepanel.tsx
@@ -1,4 +1,8 @@
+import { useParams } from "next/navigation";
+
+import { ApplicationReactQueryAdapter } from "@/core/application/react-query-adapter/application";
 import { ContributionReactQueryAdapter } from "@/core/application/react-query-adapter/contribution";
+import { ProjectReactQueryAdapter } from "@/core/application/react-query-adapter/project";
 import { UserReactQueryAdapter } from "@/core/application/react-query-adapter/user";
 
 import { Button } from "@/design-system/atoms/button/variants/button-default";
@@ -12,18 +16,60 @@ import { Timeline } from "@/shared/panels/contribution-sidepanel/_features/timel
 import { useContributionsSidepanel } from "@/shared/panels/contribution-sidepanel/contributions-sidepanel.hooks";
 import { ContributionsPanelData } from "@/shared/panels/contribution-sidepanel/contributions-sidepanel.types";
 
+import { ApplicationsAccordion } from "./_features/applications-accordion/applications-accordion";
 import { Header } from "./_features/header/header";
 import { Kpi } from "./_features/kpi/kpi";
 
 export function ContributionsSidepanel() {
+  const { projectSlug = "" } = useParams<{ projectSlug: string }>();
+
   const { name } = useContributionsSidepanel();
   const { Panel, isOpen } = useSidePanel({ name });
   const { id } = useSinglePanelData<ContributionsPanelData>(name) ?? {
     id: "",
   };
 
+  const { data: projectData } = ProjectReactQueryAdapter.client.useGetProjectBySlug({
+    pathParams: {
+      slug: projectSlug,
+    },
+    options: {
+      enabled: isOpen && !!projectSlug,
+    },
+  });
+
   const { data: pixelfactProfileData } = UserReactQueryAdapter.client.useGetUserByLogin({
     pathParams: { slug: "pixelfact" },
+  });
+
+  const { data: applicationsActiveData } = ApplicationReactQueryAdapter.client.useGetApplications({
+    queryParams: {
+      projectId: projectData?.id,
+      isApplicantProjectMember: true,
+    },
+    options: {
+      enabled: isOpen && !!projectData?.id,
+    },
+  });
+
+  const { data: applicationsNewData } = ApplicationReactQueryAdapter.client.useGetApplications({
+    queryParams: {
+      projectId: projectData?.id,
+      isApplicantProjectMember: false,
+    },
+    options: {
+      enabled: isOpen && !!projectData?.id,
+    },
+  });
+
+  const { data: applicationsIgnoredData } = ApplicationReactQueryAdapter.client.useGetApplications({
+    queryParams: {
+      projectId: projectData?.id,
+      isIgnored: true,
+    },
+    options: {
+      enabled: isOpen && !!projectData?.id,
+    },
   });
 
   const { data: contribution } = ContributionReactQueryAdapter.client.useGetContributionById({
@@ -33,6 +79,14 @@ export function ContributionsSidepanel() {
     },
   });
 
+  const activeApplicantsCount = applicationsActiveData?.pages[0].totalItemNumber ?? 0;
+  const newApplicantsCount = applicationsNewData?.pages[0].totalItemNumber ?? 0;
+  const ignoredApplicantsCount = applicationsIgnoredData?.pages[0].totalItemNumber ?? 0;
+
+  const activeApplicants = applicationsActiveData?.pages.flatMap(page => page.applications) || [];
+  const newApplicants = applicationsNewData?.pages.flatMap(page => page.applications) || [];
+  const ignoredApplicants = applicationsIgnoredData?.pages.flatMap(page => page.applications) || [];
+
   return (
     <>
       <Panel>
@@ -41,7 +95,19 @@ export function ContributionsSidepanel() {
           {id}
           <Timeline id={id} />
           <LinkedIssues issues={contribution?.linkedIssues} id={id} />
-          <Kpi applicants={2} projectContributors={10} newContributors={8} />
+          <Kpi
+            applicants={activeApplicantsCount + newApplicantsCount + ignoredApplicantsCount}
+            projectContributors={activeApplicantsCount}
+            newContributors={newApplicantsCount}
+          />
+          <ApplicationsAccordion
+            activeApplicants={activeApplicants}
+            activeApplicantsCount={activeApplicantsCount}
+            newApplicants={newApplicants}
+            newApplicantsCount={newApplicantsCount}
+            ignoredApplicants={ignoredApplicants}
+            ignoredApplicantsCount={ignoredApplicantsCount}
+          />
           <div>
             {pixelfactProfileData ? (
               <ProfileCard
