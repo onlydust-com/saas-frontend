9032bc8fe478c73ecbe4704ec5d275002b35fba2
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-10-11 15:22:32 +0200

    add contribution type filter

--- shared/features/filters/_translations/filters.translate.ts
+++ shared/features/filters/_translations/filters.translate.ts
@@ -2,6 +2,7 @@ import enAverageRewardCount from "../average-reward-count-filter/average-reward-
 import enBudgetAvailableCount from "../budget-available-count-filter/budget-available-count-filter.en.json";
 import enBudgetUsedCount from "../budget-used-count-filter/budget-used-count-filter.en.json";
 import enCategory from "../category-filter/category-filter.en.json";
+import enContributionType from "../contribution-type-filter/contribution-type-filter.en.json";
 import enContributionActivity from "../contributions-activity-filter/contributions-activity-filter.en.json";
 import enContributorLabel from "../contributor-label-filter/contributor-label-filter.en.json";
 import enContributorProject from "../contributor-project-filter/contributor-project-filter.en.json";
@@ -35,6 +36,7 @@ export const enFiltersTranslations = {
     country: enCountry,
     category: enCategory,
     userType: enUserType,
+    contributionType: enContributionType,
     rewarded: enRewarded,
     contributionActivity: enContributionActivity,
     prMergedCount: enPrMergedCount,
