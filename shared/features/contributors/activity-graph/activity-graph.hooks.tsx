d3cb5e93febafff870b6b0dc42e389f7d139f51b
Alexis Benoliel <alexbenoliel@gmail.com>
2024-10-14 10:45:34 +0200

    Update date kernel to map all date fns function

--- shared/features/contributors/activity-graph/activity-graph.hooks.tsx
+++ shared/features/contributors/activity-graph/activity-graph.hooks.tsx
@@ -1,71 +1,52 @@
-import {
-  addDays,
-  addYears,
-  eachDayOfInterval,
-  eachMonthOfInterval,
-  endOfMonth,
-  isSameMonth,
-  startOfMonth,
-  subYears,
-} from "date-fns";
+import { bootstrap } from "@/core/bootstrap";
 
 import { ActivityGraphConfig } from "@/shared/features/contributors/activity-graph/activity-graph.constants";
 
 const useActivityGraphRange = ({ start, end }: { start?: Date; end?: Date }) => {
+  const dateKernel = bootstrap.getDateKernelPort();
   if (start) {
     return {
       from: start,
-      to: addYears(addDays(start, 1), ActivityGraphConfig.number_of_years),
+      to: dateKernel.addYears(dateKernel.addDays(start, 1), ActivityGraphConfig.number_of_years),
     };
   }
 
   if (end) {
     return {
-      from: subYears(addDays(end, 1), ActivityGraphConfig.number_of_years),
+      from: dateKernel.subYears(dateKernel.addDays(end, 1), ActivityGraphConfig.number_of_years),
       to: end,
     };
   }
 
   return {
-    from: subYears(addDays(new Date(), 1), ActivityGraphConfig.number_of_years),
+    from: dateKernel.subYears(dateKernel.addDays(new Date(), 1), ActivityGraphConfig.number_of_years),
     to: new Date(),
   };
 };
 
 const useActivityGraphGrid = ({ from, to }: { from: Date; to: Date }): { months: { month: Date; days: Date[] }[] } => {
-  const months = eachMonthOfInterval({
-    start: from,
-    end: to,
-  });
+  const dateKernel = bootstrap.getDateKernelPort();
+  const months = dateKernel.eachMonthOfInterval(from, to);
 
   return {
     months: months?.map(month => {
-      if (isSameMonth(month, to)) {
+      if (dateKernel.isSameMonth(month, to)) {
         return {
           month,
-          days: eachDayOfInterval({
-            start: startOfMonth(month),
-            end: to,
-          }),
+          days: dateKernel.eachDayOfInterval(dateKernel.startOfMonth(month), to),
         };
       }
 
-      if (isSameMonth(month, from)) {
+      if (dateKernel.isSameMonth(month, from)) {
         return {
           month,
-          days: eachDayOfInterval({
-            start: from,
-            end: endOfMonth(month),
-          }),
+          days: dateKernel.eachDayOfInterval(from, dateKernel.endOfMonth(month)),
         };
       }
 
       return {
         month,
-        days: eachDayOfInterval({
-          start: startOfMonth(month),
-          end: endOfMonth(month),
-        }),
+        days: dateKernel.eachDayOfInterval(dateKernel.startOfMonth(month), dateKernel.endOfMonth(month)),
       };
     }),
   };
