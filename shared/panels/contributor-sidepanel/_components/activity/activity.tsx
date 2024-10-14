12302e8d01f1ae5a4573b9f12223de7fc5d2b475
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-10-14 16:08:22 +0200

    fix build

--- shared/panels/contributor-sidepanel/_components/activity/activity.tsx
+++ shared/panels/contributor-sidepanel/_components/activity/activity.tsx
@@ -1,24 +1,16 @@
-import { Medal } from "lucide-react";
-import { useMemo } from "react";
-
 import { UserReactQueryAdapter } from "@/core/application/react-query-adapter/user";
-import { bootstrap } from "@/core/bootstrap";
 
 import { Paper } from "@/design-system/atoms/paper";
 import { Skeleton } from "@/design-system/atoms/skeleton";
 import { Typo } from "@/design-system/atoms/typo";
 
 // import { ActivityGraph as ActivityGraphV1 } from "@/shared/features/activity-graph/activity-graph";
-import { ActivityGraphWeeksData } from "@/shared/features/activity-graph/activity-graph.types";
-import { getLevelFromCount } from "@/shared/features/activity-graph/utils/getLevelFromCount";
-import { getLevelRange } from "@/shared/features/activity-graph/utils/getLevelRange";
-import { getWeekId } from "@/shared/features/activity-graph/utils/getWeekId";
 import { ActivityGraph } from "@/shared/features/contributors/activity-graph/activity-graph";
 
 import { ActivityProps } from "./activity.types";
 
 export function Activity({ githubId }: ActivityProps) {
-  const dateKernelPort = bootstrap.getDateKernelPort();
+  // const dateKernelPort = bootstrap.getDateKernelPort();
   const { data, isLoading } = UserReactQueryAdapter.client.useGetUserStats({
     pathParams: { githubId },
     options: {
@@ -26,24 +18,24 @@ export function Activity({ githubId }: ActivityProps) {
     },
   });
 
-  const weekData = useMemo(() => {
-    const _weekData: ActivityGraphWeeksData<unknown> = {};
-
-    const levelRange = getLevelRange(
-      data?.activity?.map(activity => activity.issueCount + activity.codeReviewCount + activity.pullRequestCount) || []
-    );
-
-    data?.activity.forEach(activity => {
-      const contributionCount = activity.issueCount + activity.codeReviewCount + activity.pullRequestCount;
-      _weekData[getWeekId(dateKernelPort.getDateFromWeekNumber(activity.year, activity.week))] = {
-        level: getLevelFromCount(levelRange, contributionCount),
-        icon: activity.rewardCount > 0 ? { component: Medal } : undefined,
-        tooltipContent: `${contributionCount} contributions • ${activity.rewardCount} rewards`,
-      };
-    });
-
-    return _weekData;
-  }, [data]);
+  // const weekData = useMemo(() => {
+  //   const _weekData: ActivityGraphWeeksData<unknown> = {};
+  //
+  //   const levelRange = getLevelRange(
+  //     data?.activity?.map(activity => activity.issueCount + activity.codeReviewCount + activity.pullRequestCount) || []
+  //   );
+  //
+  //   data?.activity.forEach(activity => {
+  //     const contributionCount = activity.issueCount + activity.codeReviewCount + activity.pullRequestCount;
+  //     _weekData[getWeekId(dateKernelPort.getDateFromWeekNumber(activity.year, activity.week))] = {
+  //       level: getLevelFromCount(levelRange, contributionCount),
+  //       icon: activity.rewardCount > 0 ? { component: Medal } : undefined,
+  //       tooltipContent: `${contributionCount} contributions • ${activity.rewardCount} rewards`,
+  //     };
+  //   });
+  //
+  //   return _weekData;
+  // }, [data]);
 
   if (isLoading) {
     return <Skeleton className={"h-[170px] w-full"} />;
