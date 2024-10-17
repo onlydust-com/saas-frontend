e3c1f55afc0b5d708d10afcf765213b3f405f6b3
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-10-17 18:10:18 +0200

    add github permission modal

---
+++ shared/modals/github-permission-modal/github-permission-modal.types.ts
@@ -0,0 +1,5 @@
+import { ModalPort } from "@/design-system/molecules/modal";
+
+export interface GithubPermissionModalProps extends Pick<ModalPort<"div">, "isOpen" | "onOpenChange"> {
+  redirectionUrl: string;
+}
