28fbb163a102b3871f28464b59b740ccd64b01fa
Alexis Benoliel <alexbenoliel@gmail.com>
2024-10-19 12:14:47 +0200

    Add maxHeight instead of height for contribution scroll view

--- shared/components/scroll-view/scroll-view.tsx
+++ shared/components/scroll-view/scroll-view.tsx
@@ -5,12 +5,13 @@ import { cn } from "@/shared/helpers/cn";
 import { ScrollViewProps } from "./scroll-view.types";
 
 export const ScrollView = forwardRef(function ScrollView(
-  { className, children, direction = "y" }: ScrollViewProps,
+  { className, children, direction = "y", style }: ScrollViewProps,
   ref: ForwardedRef<HTMLDivElement>
 ) {
   return (
     <div
       ref={ref}
+      style={style}
       className={cn(
         "scrollbar-thumb-white/12 scrollbar-thumb-rounded scrollbar-w-1.5 size-full scrollbar-thin",
         {
