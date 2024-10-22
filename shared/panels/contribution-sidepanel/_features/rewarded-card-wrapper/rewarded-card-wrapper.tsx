09de5d3dec5162c9bd1877b8c9b95f4bf976a9cc
Alexis Benoliel <alexbenoliel@gmail.com>
2024-10-22 15:36:19 +0200

    Update rewarded-card-wrapper.tsx

--- shared/panels/contribution-sidepanel/_features/rewarded-card-wrapper/rewarded-card-wrapper.tsx
+++ shared/panels/contribution-sidepanel/_features/rewarded-card-wrapper/rewarded-card-wrapper.tsx
@@ -1,18 +1,23 @@
+import { useMemo } from "react";
+
 import { RewardReactQueryAdapter } from "@/core/application/react-query-adapter/reward";
 
 import { RewardedCard } from "@/shared/panels/contribution-sidepanel/_features/rewarded-card/rewarded-card";
 
 import { RewardedCardWrapperProps } from "./rewarded-card-wrapper.types";
 
-export function SafeRewardedCardWrapper({ contribution }: RewardedCardWrapperProps) {
+export function RewardedCardWrapper({ contribution }: RewardedCardWrapperProps) {
   const { data } = RewardReactQueryAdapter.client.useGetRewards({
     queryParams: {
       contributionUUIDs: [contribution.id],
       pageSize: 30,
     },
+    options: {
+      enabled: !!contribution?.totalRewardedUsdAmount,
+    },
   });
 
-  const rewards = data?.pages.flatMap(page => page.rewards) || [];
+  const rewards = useMemo(() => data?.pages.flatMap(page => page.rewards) || [], [data]);
 
   if (!rewards.length) {
     return null;
@@ -21,12 +26,4 @@ export function SafeRewardedCardWrapper({ contribution }: RewardedCardWrapperPro
   return rewards?.map((reward, index) => (
     <RewardedCard key={`rewarded-card-${index}`} reward={reward.amount} processedAt={reward.processedAt} />
   ));
-}
-
-export function RewardedCardWrapper({ contribution }: RewardedCardWrapperProps) {
-  if (!contribution?.totalRewardedUsdAmount) {
-    return null;
-  }
-
-  return <SafeRewardedCardWrapper contribution={contribution} />;
 }
