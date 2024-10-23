7f88dd6fe5e41423ecd22d30fe2f8a00a1885b36
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-10-23 11:14:04 +0200

    Add remove contributor button in assignees cards (#510)

--- shared/panels/contribution-sidepanel/_features/assignees/assignees.types.ts
+++ shared/panels/contribution-sidepanel/_features/assignees/assignees.types.ts
@@ -1,4 +1,6 @@
+import { ContributionActivityInterface } from "@/core/domain/contribution/models/contribution-activity-model";
+
 export interface AssigneesProps {
   showRemove?: boolean;
-  contributionId: string;
+  contribution: ContributionActivityInterface;
 }
