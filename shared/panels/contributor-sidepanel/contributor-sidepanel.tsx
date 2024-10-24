ddd0faced50ca55fba633fff41995b73e1c7b152
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-10-24 12:27:19 +0200

    add footer

--- shared/panels/contributor-sidepanel/contributor-sidepanel.tsx
+++ shared/panels/contributor-sidepanel/contributor-sidepanel.tsx
@@ -1,17 +1,24 @@
+import { SquareArrowOutUpRight } from "lucide-react";
+
 import { BiReactQueryAdapter } from "@/core/application/react-query-adapter/bi";
 
+import { Button } from "@/design-system/atoms/button/variants/button-default";
 import { Skeleton } from "@/design-system/atoms/skeleton";
 
 import { EmptyStateLite } from "@/shared/components/empty-state-lite/empty-state-lite";
+import { MARKETPLACE_ROUTER } from "@/shared/constants/router";
 import { ContributorProfileExtended } from "@/shared/features/contributors/contributor-profile-extended/contributor-profile-extended";
 import { SidePanelBody } from "@/shared/features/side-panels/side-panel-body/side-panel-body";
+import { SidePanelFooter } from "@/shared/features/side-panels/side-panel-footer/side-panel-footer";
 import { SidePanelHeader } from "@/shared/features/side-panels/side-panel-header/side-panel-header";
 import { useSidePanel, useSinglePanelData } from "@/shared/features/side-panels/side-panel/side-panel";
+import { marketplaceRouting } from "@/shared/helpers/marketplace-routing";
 import { Activity } from "@/shared/panels/contributor-sidepanel/_components/activity/activity";
 import { Ecosystems } from "@/shared/panels/contributor-sidepanel/_components/ecosystems/ecosystems";
 import { Languages } from "@/shared/panels/contributor-sidepanel/_components/languages/languages";
 import { RewardsGraph } from "@/shared/panels/contributor-sidepanel/_components/rewards-graph/rewards-graph";
 import { useContributorSidePanel } from "@/shared/panels/contributor-sidepanel/contributor-sidepanel.hooks";
+import { Translate } from "@/shared/translation/components/translate/translate";
 
 import { ContributorSidepanelData, ContributorSidepanelProps } from "./contributor-sidepanel.types";
 
@@ -27,11 +34,7 @@ export function ContributorSidepanel({ customFooter }: ContributorSidepanelProps
     applicationId: undefined,
   };
 
-  const {
-    data: data,
-    isLoading,
-    isError,
-  } = BiReactQueryAdapter.client.useGetBiContributorById({
+  const { data: data, isLoading } = BiReactQueryAdapter.client.useGetBiContributorById({
     pathParams: { contributorId: githubId },
 
     options: {
@@ -83,42 +86,42 @@ export function ContributorSidepanel({ customFooter }: ContributorSidepanelProps
     );
   }
 
-  // function renderFooter() {
-  //   if (!data) return null;
-  //
-  //   if (customFooter) {
-  //     return customFooter({ data, applicationId });
-  //   }
-  //
-  //   return (
-  //     <div className={"flex w-full flex-row items-center justify-end gap-1"}>
-  //       <Button
-  //         variant={"secondary"}
-  //         endContent={<SquareArrowOutUpRight size={16} />}
-  //         size={"md"}
-  //         as={"a"}
-  //         htmlProps={{
-  //           href: marketplaceRouting(MARKETPLACE_ROUTER.publicProfile.root(data.login)),
-  //           target: "_blank",
-  //         }}
-  //       >
-  //         <Translate token={"panels:contributor.seeContributor"} />
-  //       </Button>
-  //     </div>
-  //   );
-  // }
+  function renderFooter() {
+    if (!data) return null;
+
+    if (customFooter) {
+      return customFooter({ data, applicationId });
+    }
+
+    return (
+      <div className={"flex w-full flex-row items-center justify-end gap-1"}>
+        <Button
+          variant={"secondary"}
+          endContent={<SquareArrowOutUpRight size={16} />}
+          size={"md"}
+          as={"a"}
+          htmlProps={{
+            href: marketplaceRouting(MARKETPLACE_ROUTER.publicProfile.root(data.contributor.login)),
+            target: "_blank",
+          }}
+        >
+          <Translate token={"panels:contributor.seeContributor"} />
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <Panel>
       <SidePanelHeader
-        // title={{
-        //   children: data?.login ?? "",
-        // }}
+        title={{
+          children: data?.contributor.login ?? "",
+        }}
         canGoBack={canGoBack}
         canClose={true}
       />
       <SidePanelBody>{renderContent()}</SidePanelBody>
-      {/*{data ? <SidePanelFooter>{renderFooter()}</SidePanelFooter> : null}*/}
+      {data ? <SidePanelFooter>{renderFooter()}</SidePanelFooter> : null}
     </Panel>
   );
 }
