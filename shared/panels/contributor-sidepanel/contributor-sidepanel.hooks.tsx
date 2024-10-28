5df5f9383127fbc7c3cb54449c9b4ff7a631d99a
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-10-28 12:12:15 +0100

    E 2310 snack ouvrir le sidepanel du contributor (#536)

--- shared/panels/contributor-sidepanel/contributor-sidepanel.hooks.tsx
+++ shared/panels/contributor-sidepanel/contributor-sidepanel.hooks.tsx
@@ -1,11 +1,7 @@
-import { SIDE_PANEL_SIZE } from "@/shared/constants/side-panel-size";
 import { useSinglePanelContext } from "@/shared/features/side-panels/side-panel/side-panel";
 import { SidePanelConfig } from "@/shared/features/side-panels/side-panels.types";
 import { ContributorSidepanelData } from "@/shared/panels/contributor-sidepanel/contributor-sidepanel.types";
 
 export function useContributorSidePanel(config?: SidePanelConfig) {
-  return useSinglePanelContext<ContributorSidepanelData>("contributor-detail", {
-    width: SIDE_PANEL_SIZE["3xl"],
-    ...config,
-  });
+  return useSinglePanelContext<ContributorSidepanelData>("contributor-detail", config);
 }
