bbadd8d26accbd9c191d82da80a1109368d687b0
Sami Lafrance <sami.lepays@gmail.com>
2024-10-28 17:38:35 +0100

    Update table sort (#537)

--- design-system/molecules/table-sort/adapters/default/default.variants.ts
+++ design-system/molecules/table-sort/adapters/default/default.variants.ts
@@ -2,8 +2,16 @@ import { tv } from "tailwind-variants";
 
 export const TableSortDefaultVariants = tv({
   slots: {
-    base: "",
+    base: "h-4 w-4 rounded p-xxs text-foreground-quaternary transition-colors hover:bg-background-tertiary hover:text-foreground-secondary-hover",
   },
-  variants: {},
-  defaultVariants: {},
+  variants: {
+    isSelected: {
+      true: {
+        base: "text-foreground-brand-primary hover:bg-background-brand-primary-alt hover:text-foreground-brand-primary-hover",
+      },
+    },
+  },
+  defaultVariants: {
+    isSelected: false,
+  },
 });
