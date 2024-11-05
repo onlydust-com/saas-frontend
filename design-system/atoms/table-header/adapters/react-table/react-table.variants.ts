40176bc87b2c38679b7556cd305dece6cff1d714
Sami Lafrance <sami.lepays@gmail.com>
2024-11-05 12:32:38 +0100

    fix(header): width

--- design-system/atoms/table-header/adapters/react-table/react-table.variants.ts
+++ design-system/atoms/table-header/adapters/react-table/react-table.variants.ts
@@ -6,7 +6,7 @@ export const TableHeaderReactTableVariants = tv({
     row: "",
     header:
       "relative whitespace-nowrap border-b-1 border-border-primary px-lg py-sm text-left leading-none after:absolute after:right-0 after:top-1/2 after:block after:h-4 after:w-px after:-translate-y-1/2 after:bg-border-primary after:content-[''] last:after:hidden",
-    headerInner: "group/header-inner flex items-center gap-md rounded-sm p-sm transition-all",
+    headerInner: "group/header-inner flex w-fit items-center gap-md rounded-sm p-sm transition-all",
   },
   variants: {},
   defaultVariants: {},
