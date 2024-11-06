3546432b1151ba104b14ba79d7eb308f46c3094c
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-11-06 11:24:26 +0100

    refactor: nav banner styles

--- design-system/organisms/plg-banner/plg-banner.tsx
+++ design-system/organisms/plg-banner/plg-banner.tsx
@@ -29,37 +29,40 @@ export function PlgBanner({ title, subTitle, date, description, cta }: PlgBanner
   return (
     <div
       className={
-        "relative flex h-auto max-h-full w-full flex-col overflow-hidden rounded-xl border border-border-primary p-6"
+        "relative flex h-auto max-h-full w-full flex-col overflow-hidden rounded-xl border border-border-primary p-xl"
       }
     >
       <div className="absolute inset-0 gradient-glass-neon-80" />
 
       <div className="relative flex h-auto max-h-full w-full flex-col gap-2.5">
-        <div className={"flex flex-col gap-8"}>
-          <div className={"flex flex-col gap-2"}>
+        <div className={"flex flex-col gap-xl"}>
+          <div className={"flex flex-col gap-xl"}>
             <div className={"flex w-full items-center justify-between"}>
               {title ? (
-                <Typo size={"sm"} weight={"regular"} color={"tertiary"}>
+                <Typo size={"xs"} weight={"regular"} color={"tertiary"}>
                   {title}
                 </Typo>
               ) : null}
               {date ? (
-                <Typo size={"sm"} weight={"regular"} color={"tertiary"}>
+                <Typo size={"xs"} weight={"regular"} color={"tertiary"}>
                   {date}
                 </Typo>
               ) : null}
             </div>
-            {subTitle ? (
-              <Typo size={"md"} color={"secondary"}>
-                {subTitle}
-              </Typo>
-            ) : null}
 
-            {description ? (
-              <Typo size={"sm"} weight={"regular"} color={"tertiary"}>
-                {description}
-              </Typo>
-            ) : null}
+            <div className={"flex flex-col gap-sm"}>
+              {subTitle ? (
+                <Typo variant={"heading"} size={"xs"} color={"secondary"}>
+                  {subTitle}
+                </Typo>
+              ) : null}
+
+              {description ? (
+                <Typo size={"xs"} weight={"regular"} color={"tertiary"}>
+                  {description}
+                </Typo>
+              ) : null}
+            </div>
           </div>
 
           <Cta cta={cta} />
