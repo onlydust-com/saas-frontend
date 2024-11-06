7a80dbf86b3a0323dfcad4721e3470277bfef636
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-11-06 09:57:44 +0100

    Add datasource filter in url in data section (#577)

--- app/data/_sections/data-section/components/histograms/contributor-histogram-chart/contributor-histogram-chart.tsx
+++ app/data/_sections/data-section/components/histograms/contributor-histogram-chart/contributor-histogram-chart.tsx
@@ -154,6 +154,7 @@ export function ContributorHistogramChart() {
             placeholder={t("data:details.allDataFilter.placeholder")}
             onSelect={onProgramEcosystemChange}
             selectedProgramsEcosystems={selectedProgramAndEcosystem}
+            searchParams={"programAndEcosystemIds"}
           />
           <Menu items={rangeMenu} selectedIds={[rangeType]} onAction={onChangeRangeType} isPopOver>
             <Button
