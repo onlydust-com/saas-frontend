5b2ed01d4f9e5f356dfd60422ef0a9083c5ffcd7
Sami Lafrance <sami.lepays@gmail.com>
2024-11-06 12:05:43 +0100

    fix(menu): gap

--- design-system/molecules/menu/adapters/default/default.variants.ts
+++ design-system/molecules/menu/adapters/default/default.variants.ts
@@ -4,7 +4,7 @@ export const MenuDefaultVariants = tv({
   slots: {
     base: "gap-0 p-0",
     content:
-      "max-h-60 min-w-[160px] justify-start gap-0 overflow-auto rounded-md border border-border-primary bg-background-primary p-0 py-xs effect-box-shadow-lg scrollbar",
+      "flex max-h-60 min-w-[160px] flex-col justify-start gap-xs overflow-auto rounded-md border border-border-primary bg-background-primary p-0 py-xs effect-box-shadow-lg scrollbar",
   },
   variants: {},
   defaultVariants: {},
