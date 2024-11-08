6334ef679eb2fd69de201df68be819f7dba838d9
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-11-08 09:38:22 +0100

    fix contract

--- core/domain/billing-profile/billing-profile-contract.types.ts
+++ core/domain/billing-profile/billing-profile-contract.types.ts
@@ -1,3 +1,5 @@
+import { BillingProfileInterface } from "@/core/domain/billing-profile/models/billing-profile-model";
+import { BillingProfilePayoutInfoInterface } from "@/core/domain/billing-profile/models/billing-profile-payout-info-model";
 import { components, operations } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
 import {
   HttpClientParameters,
@@ -8,7 +10,7 @@ import {
 
 export type GetBillingProfileByIdResponse = components["schemas"]["BillingProfileResponse"];
 
-export type GetBillingProfileByIdPortResponse = HttpStorageResponse<GetBillingProfileByIdResponse>;
+export type GetBillingProfileByIdModel = BillingProfileInterface;
 
 type GetBillingProfileByIdPathParams = operations["getProgram"]["parameters"]["path"];
 
@@ -16,14 +18,18 @@ export type GetBillingProfileByIdPortParams = HttpClientParameters<{
   PathParams: GetBillingProfileByIdPathParams;
 }>;
 
+export type GetBillingProfileByIdPortResponse = HttpStorageResponse<GetBillingProfileByIdModel>;
+
 /* ------------------------------ Get Billing Profile Payout Info by ID ------------------------------ */
 
 export type GetBillingProfilePayoutInfoByIdResponse = components["schemas"]["BillingProfilePayoutInfoResponse"];
 
-export type GetBillingProfilePayoutInfoByIdPortResponse = HttpStorageResponse<GetBillingProfilePayoutInfoByIdResponse>;
+export type GetBillingProfilePayoutInfoByIdModel = BillingProfilePayoutInfoInterface;
 
 type GetBillingProfilePayoutInfoByIdPathParams = operations["getProgram"]["parameters"]["path"];
 
 export type GetBillingProfilePayoutInfoByIdPortParams = HttpClientParameters<{
   PathParams: GetBillingProfilePayoutInfoByIdPathParams;
 }>;
+
+export type GetBillingProfilePayoutInfoByIdPortResponse = HttpStorageResponse<GetBillingProfilePayoutInfoByIdModel>;
