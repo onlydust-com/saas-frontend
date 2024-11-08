671b162a5ae2f11970637bfea8cf7966cf6add32
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-11-08 15:25:32 +0100

    WIP add invoice tools and components

---
+++ core/domain/billing-profile/billing-profile.types.ts
@@ -0,0 +1,4 @@
+import { components } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
+
+export type VatRegulationState =
+  components["schemas"]["InvoicePreviewResponseCompanyBillingProfile"]["vatRegulationState"];
---
+++ shared/features/invoice/viewer/invoice-viewer.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+import { cn } from "@/shared/helpers/cn";
+
+import "../styles/annotation-layer.css";
+import "../styles/text-layer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.js", import.meta.url).toString();
+
+export default function InvoiceViewer({ fileUrl, className }: { fileUrl: string; className?: string }) {
+  const [numPages, setNumPages] = useState(0);
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <Document className={cn("w-fit", className)} file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
+      {Array.from(new Array(numPages), (_el, index) => (
+        <Page key={`page_${index + 1}`} pageNumber={index + 1} className="mb-2" />
+      ))}
+    </Document>
+  );
+}
