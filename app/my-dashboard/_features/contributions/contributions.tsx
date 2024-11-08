a9ed26ecea6f1958f2c3906747b029b11e8cd76f
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-11-08 10:58:53 +0100

    refactor: dashboard contributions removed list view

--- app/my-dashboard/_features/contributions/contributions.tsx
+++ app/my-dashboard/_features/contributions/contributions.tsx
@@ -1,11 +1,9 @@
-import { Columns4, Table } from "lucide-react";
 import { useParams } from "next/navigation";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
 import { FilterData } from "@/app/my-dashboard/_features/contributions/filter-data/filter-data";
 import { useContributorContributionsFilterDataSidePanel } from "@/app/my-dashboard/_features/contributions/filter-data/filter-data.hooks";
 import { KanbanView } from "@/app/my-dashboard/_features/contributions/kanban-view/kanban-view";
-import { ListView } from "@/app/my-dashboard/_features/contributions/list-view/list-view";
 
 import {
   GetContributionsPortParams,
@@ -13,18 +11,11 @@ import {
 } from "@/core/domain/contribution/contribution-contract.types";
 import { ContributionAs } from "@/core/domain/contribution/models/contribution.types";
 
-import { Icon } from "@/design-system/atoms/icon";
 import { TableSearch } from "@/design-system/molecules/table-search";
-import { Tabs } from "@/design-system/molecules/tabs/tabs";
 
 import { FilterButton } from "@/shared/features/filters/_components/filter-button/filter-button";
 import { FilterDataProvider } from "@/shared/features/filters/_contexts/filter-data/filter-data.context";
 import { useContributionsSidepanel } from "@/shared/panels/contribution-sidepanel/contributions-sidepanel.hooks";
-
-enum View {
-  "LIST" = "LIST",
-  "KANBAN" = "KANBAN",
-}
 
 export type ContributionKanbanFilters = Omit<
   NonNullable<GetContributionsPortParams["queryParams"]>,
@@ -32,7 +23,6 @@ export type ContributionKanbanFilters = Omit<
 >;
 
 export function Contributions() {
-  const [toggleViews, setToggleViews] = useState<View>(View.KANBAN);
   const [filters, setFilters] = useState<ContributionKanbanFilters>({});
   const [search, setSearch] = useState<string>();
   const [debouncedSearch, setDebouncedSearch] = useState<string>();
@@ -50,25 +40,9 @@ export function Contributions() {
     ...filters,
   };
 
-  function handleToggleViews(view: string) {
-    setToggleViews(view as View);
-  }
-
   function onOpenContribution(id: string) {
     openContribution({ id, as: ContributionAs.CONTRIBUTOR });
   }
-
-  const renderView = useMemo(() => {
-    if (toggleViews === View.LIST) {
-      return <ListView queryParams={queryParams} onOpenContribution={onOpenContribution} />;
-    }
-
-    if (toggleViews === View.KANBAN) {
-      return <KanbanView queryParams={queryParams} onOpenContribution={onOpenContribution} />;
-    }
-
-    return null;
-  }, [toggleViews, queryParams]);
 
   return (
     <FilterDataProvider filters={filters} setFilters={setFilters}>
@@ -77,25 +51,11 @@ export function Contributions() {
           <FilterButton onClick={openFilterPanel} />
 
           <TableSearch value={search} onChange={setSearch} onDebouncedChange={setDebouncedSearch} />
-
-          <Tabs
-            onTabClick={handleToggleViews}
-            variant="solid"
-            tabs={[
-              {
-                id: View.LIST,
-                children: <Icon component={Table} />,
-              },
-              {
-                id: View.KANBAN,
-                children: <Icon component={Columns4} />,
-              },
-            ]}
-            selectedId={toggleViews}
-          />
         </nav>
 
-        <div className={"h-full overflow-hidden"}>{renderView}</div>
+        <div className={"h-full overflow-hidden"}>
+          <KanbanView queryParams={queryParams} onOpenContribution={onOpenContribution} />
+        </div>
       </div>
 
       <FilterData />
--- app/my-dashboard/_features/contributions/list-view/list-view.types.ts
+++
@@ -1,6 +0,0 @@
-import { GetContributionsQueryParams } from "@/core/domain/contribution/contribution-contract.types";
-
-export interface ListViewProps {
-  queryParams: Partial<GetContributionsQueryParams>;
-  onOpenContribution(id: string): void;
-}
