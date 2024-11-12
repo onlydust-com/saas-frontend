357619949b6d22c990ece14528dcf7c5004469a2
Alexis Benoliel <alexbenoliel@gmail.com>
2024-11-12 16:42:34 +0100

    Add payout status on rewards columns

--- shared/features/payout-status/payout-status.tsx
+++ shared/features/payout-status/payout-status.tsx
@@ -1,18 +1,56 @@
-import { PayoutStatusMapping } from "@/core/domain/reward/reward-constants";
+import { PayoutStatusContent, PayoutStatusMapping } from "@/core/domain/reward/reward-constants";
+import { AnyType } from "@/core/kernel/types";
 
-import { Badge } from "@/design-system/atoms/badge";
+import { Badge, BadgePort } from "@/design-system/atoms/badge";
 import { Icon } from "@/design-system/atoms/icon";
 
+import { MARKETPLACE_ROUTER } from "@/shared/constants/router";
 import { PayoutStatusProps } from "@/shared/features/payout-status/payout-status.types";
+import { marketplaceRouting } from "@/shared/helpers/marketplace-routing";
 
-export function PayoutStatus({ status }: PayoutStatusProps) {
+export function PayoutStatus({ status, billingProfileId, shouldRedirect }: PayoutStatusProps) {
+  function getRedirectLink() {
+    if (!billingProfileId || !shouldRedirect) return undefined;
+
+    return marketplaceRouting(MARKETPLACE_ROUTER.settings.billing.generalInformation(billingProfileId));
+  }
+
+  const propsMapping: Record<PayoutStatusContent["type"], Partial<BadgePort<AnyType>>> = {
+    error: {
+      color: "error",
+      shape: "squared",
+      variant: "outline",
+      as: "a",
+      htmlProps: {
+        href: getRedirectLink(),
+        target: "_blank",
+        rel: "noopener noreferrer",
+      },
+    },
+    warning: {
+      color: "grey",
+      shape: "squared",
+      as: "a",
+      htmlProps: {
+        href: getRedirectLink(),
+        target: "_blank",
+        rel: "noopener noreferrer",
+      },
+    },
+    success: {
+      color: "grey",
+    },
+  };
+
+  const badgeProps = propsMapping[PayoutStatusMapping[status].type];
+
   return (
     <Badge
       size={"md"}
-      color={"grey"}
       translate={{ token: PayoutStatusMapping[status].label }}
       startContent={<Icon {...PayoutStatusMapping[status].icon} classNames={{ base: "text-text-2" }} />}
       classNames={{ base: "w-fit" }}
+      {...badgeProps}
     />
   );
 }
