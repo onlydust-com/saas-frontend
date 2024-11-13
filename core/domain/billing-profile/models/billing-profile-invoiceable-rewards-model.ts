b101bd22c2a9aebecbddc80d0af8f624af332707
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-11-13 10:11:25 +0100

    B 2733 create request payment flow context (#629)
    Co-authored-by: Hayden Cleary <5160414+haydencleary@users.noreply.github.com>

---
+++ core/domain/billing-profile/models/billing-profile-invoiceable-rewards-model.ts
@@ -0,0 +1,25 @@
+import { components } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
+
+export type BillingProfileInvoiceableRewardResponse = components["schemas"]["MyRewardPageItemResponse"];
+
+export interface BillingProfileInvoiceableRewardInterface extends BillingProfileInvoiceableRewardResponse {}
+
+export class BillingProfileInvoiceableReward implements BillingProfileInvoiceableRewardInterface {
+  amount!: BillingProfileInvoiceableRewardResponse["amount"];
+  billingProfileId!: BillingProfileInvoiceableRewardResponse["billingProfileId"];
+  id!: BillingProfileInvoiceableRewardResponse["id"];
+  networks!: BillingProfileInvoiceableRewardResponse["networks"];
+  numberOfRewardedContributions!: BillingProfileInvoiceableRewardResponse["numberOfRewardedContributions"];
+  processedAt!: BillingProfileInvoiceableRewardResponse["processedAt"];
+  projectId!: BillingProfileInvoiceableRewardResponse["projectId"];
+  requestedAt!: BillingProfileInvoiceableRewardResponse["requestedAt"];
+  rewardedOnProjectLogoUrl!: BillingProfileInvoiceableRewardResponse["rewardedOnProjectLogoUrl"];
+  rewardedOnProjectName!: BillingProfileInvoiceableRewardResponse["rewardedOnProjectName"];
+  rewardedUser!: BillingProfileInvoiceableRewardResponse["rewardedUser"];
+  status!: BillingProfileInvoiceableRewardResponse["status"];
+  unlockDate!: BillingProfileInvoiceableRewardResponse["unlockDate"];
+
+  constructor(props: BillingProfileInvoiceableRewardResponse) {
+    Object.assign(this, props);
+  }
+}
