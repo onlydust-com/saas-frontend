09649dd5b1f07175730c3ce891f8df2e5420d73a
Alexis Benoliel <alexbenoliel@gmail.com>
2024-11-15 17:13:03 +0100

    add timeline blocks

--- shared/panels/reward-detail-sidepanel/_features/reward-timeline/reward-timeline.tsx
+++ shared/panels/reward-detail-sidepanel/_features/reward-timeline/reward-timeline.tsx
@@ -1,29 +1,70 @@
-import { Loader, TriangleAlert } from "lucide-react";
+import { useClipboard } from "@nextui-org/use-clipboard";
+import { Check, Copy, Loader, TriangleAlert } from "lucide-react";
 import { useMemo } from "react";
 
 import { bootstrap } from "@/core/bootstrap";
 import { AnyType } from "@/core/kernel/types";
 
 import { Button } from "@/design-system/atoms/button/variants/button-default";
+import { Input } from "@/design-system/atoms/input";
 import { Paper } from "@/design-system/atoms/paper";
 import { Typo } from "@/design-system/atoms/typo";
 import { Timeline as TimelineComponent, TimelinePort } from "@/design-system/organisms/timeline";
 
+import { MARKETPLACE_ROUTER } from "@/shared/constants/router";
+import { marketplaceRouting } from "@/shared/helpers/marketplace-routing";
 import { Translate } from "@/shared/translation/components/translate/translate";
 
 import { RewardTimelineProps } from "./reward-timeline.types";
 
 export function RewardTimeline({ reward }: RewardTimelineProps) {
+  const { copy } = useClipboard();
   const dateKernelPort = bootstrap.getDateKernelPort();
 
   const timelineItems: TimelinePort<AnyType>["items"] = useMemo(() => {
     const items: TimelinePort<AnyType>["items"] = [];
 
-    if (reward.status !== "PENDING_REQUEST") {
+    if (reward.processedAt) {
+      items.push({
+        label: <Translate token="panels:rewardDetail.timeline.paid.label" />,
+        endContent: dateKernelPort.format(new Date(reward.processedAt), "dd MMM yyyy"),
+        icon: { component: Check },
+        children: (
+          <Paper background={"primary"} classNames={{ base: "flex flex-col w-full gap-3" }}>
+            <Typo size={"sm"} weight={"medium"} translate={{ token: "panels:rewardDetail.timeline.paid.title" }} />
+            <Input
+              name={"reference"}
+              value={reward.transactionReference}
+              canInteract={false}
+              endContent={
+                <Button
+                  iconOnly={true}
+                  size={"xs"}
+                  variant={"tertiary"}
+                  startIcon={{ component: Copy }}
+                  onClick={() => copy(reward.transactionReference)}
+                />
+              }
+            />
+            <Button
+              variant={"secondary"}
+              translate={{ token: "panels:rewardDetail.timeline.verificationBlock.button" }}
+              as={"a"}
+              htmlProps={{
+                href: reward.transactionReferenceLink,
+                target: "_blank",
+              }}
+            />
+          </Paper>
+        ),
+      });
+    }
+
+    if (reward.isBlocked() && reward.status) {
       items.push({
         label: (
           <Translate
-            token="panels:rewardDetail.timeline.verificationBlock.label"
+            token={`panels:rewardDetail.timeline.verificationBlock.type.${reward.status}.title`}
             values={{ login: reward.from.login }}
           />
         ),
@@ -36,12 +77,47 @@ export function RewardTimeline({ reward }: RewardTimelineProps) {
             <Typo
               size={"sm"}
               weight={"medium"}
-              translate={{ token: "panels:rewardDetail.timeline.verificationBlock.content" }}
+              translate={{ token: `panels:rewardDetail.timeline.verificationBlock.type.${reward.status}.text` }}
             />
-            <Button
-              isTextButton={true}
-              translate={{ token: "panels:rewardDetail.timeline.verificationBlock.button" }}
-            />
+            {reward.status === "PENDING_BILLING_PROFILE" && (
+              <Button
+                isTextButton={true}
+                translate={{
+                  token: "panels:rewardDetail.timeline.verificationBlock.type.PENDING_BILLING_PROFILE.button",
+                }}
+                as={"a"}
+                htmlProps={{
+                  href: marketplaceRouting(MARKETPLACE_ROUTER.settings.payoutPreferences),
+                  target: "_blank",
+                }}
+              />
+            )}
+            {reward.status === "INDIVIDUAL_LIMIT_REACHED" && (
+              <Button
+                isTextButton={true}
+                translate={{
+                  token: "panels:rewardDetail.timeline.verificationBlock.type.INDIVIDUAL_LIMIT_REACHED.button",
+                }}
+                as={"a"}
+                htmlProps={{
+                  href: marketplaceRouting(MARKETPLACE_ROUTER.settings.payoutPreferences),
+                  target: "_blank",
+                }}
+              />
+            )}
+            {reward.status === "PAYOUT_INFO_MISSING" && (
+              <Button
+                isTextButton={true}
+                translate={{
+                  token: "panels:rewardDetail.timeline.verificationBlock.type.PAYOUT_INFO_MISSING.button",
+                }}
+                as={"a"}
+                htmlProps={{
+                  href: marketplaceRouting(MARKETPLACE_ROUTER.settings.payoutPreferences),
+                  target: "_blank",
+                }}
+              />
+            )}
           </Paper>
         ),
       });
