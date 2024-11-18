6f2bcbf8aa0b784866dfbff4e505633bf28043b8
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-11-18 15:53:50 +0100

    refactor: repo popover types

--- shared/features/repos/repos-popover/repos-popover.types.ts
+++ shared/features/repos/repos-popover/repos-popover.types.ts
@@ -1,10 +1,7 @@
-// TODO @hayden update this with core model
 interface Repo {
   id: number;
-  owner: string;
   name: string;
   description?: string;
-  htmlUrl: string;
 }
 
 export interface ReposPopoverProps {
