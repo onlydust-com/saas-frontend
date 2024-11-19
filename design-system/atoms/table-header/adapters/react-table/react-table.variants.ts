745dfce8bcd7a4bf8ac4bab7a9201195cdbc78bd
Alexis Benoliel <alexbenoliel@gmail.com>
2024-11-19 11:17:36 +0100

    Update react-table.variants.ts

--- design-system/atoms/table-header/adapters/react-table/react-table.variants.ts
+++ design-system/atoms/table-header/adapters/react-table/react-table.variants.ts
@@ -2,7 +2,7 @@ import { tv } from "tailwind-variants";
 
 export const TableHeaderReactTableVariants = tv({
   slots: {
-    base: "bg-transparent",
+    base: "sticky top-0 z-[1] bg-background-primary-alt",
     row: "",
     header:
       "group/header relative whitespace-nowrap border-b-1 border-border-primary px-lg py-sm text-left leading-none after:absolute after:right-2 after:top-1/2 after:block after:h-4 after:w-px after:-translate-y-1/2 after:bg-border-primary after:content-[''] last:after:hidden",
