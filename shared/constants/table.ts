0f5f942ba43f616c330c4bdcb5b0454e7b62e570
Sami Lafrance <sami.lepays@gmail.com>
2024-11-27 12:23:07 +0100

    fix(*): uncomment

--- shared/constants/table.ts
+++ shared/constants/table.ts
@@ -4,15 +4,6 @@ export const TABLE_DEFAULT_COLUMN = {
   maxSize: 700,
 } as const;
 
-// export const TABLE_CELL_SIZE = {
-//   XS: 40,
-//   SM: 80,
-//   MD: 120,
-//   LG: 160,
-//   XL: 200,
-//   XXL: 240,
-// } as const;
-
 export const TABLE_CELL_SIZE = {
   XXXS: 40,
   XXS: 80,
