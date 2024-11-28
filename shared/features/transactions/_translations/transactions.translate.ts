28e9bb53c2867ed1fb4de608520eb28f44c4e02f
Sami Lafrance <sami.lepays@gmail.com>
2024-11-28 10:34:05 +0100

    Refacto/transactions filters (#718)

--- shared/features/transactions/_translations/transactions.translate.ts
+++ shared/features/transactions/_translations/transactions.translate.ts
@@ -1,7 +1,9 @@
 import enTransactionsHeader from "../transactions-header/transactions-header.en.json";
+import enTransactionsFilters from "../transactions-filters/transactions-filters.en.json";
 
 export const enTransactionsTranslation = {
   transactions: {
     header: enTransactionsHeader,
+    filters: enTransactionsFilters,
   },
 };
