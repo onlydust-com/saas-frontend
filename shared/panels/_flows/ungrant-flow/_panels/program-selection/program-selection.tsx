7eabae5d342eda39f79a2d3f46b1a736ecf3d4ec
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-12-03 09:55:25 +0100

    feat: program search

--- shared/panels/_flows/ungrant-flow/_panels/program-selection/program-selection.tsx
+++ shared/panels/_flows/ungrant-flow/_panels/program-selection/program-selection.tsx
@@ -1,9 +1,16 @@
+import { Search } from "lucide-react";
+import { useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useDebounce } from "react-use";
+
 import { ProjectReactQueryAdapter } from "@/core/application/react-query-adapter/project";
 import { ProjectProgramListItemInterface } from "@/core/domain/project/models/project-program-list-item";
 
+import { Icon } from "@/design-system/atoms/icon";
+import { Input } from "@/design-system/atoms/input";
 import { CardProjectLoading } from "@/design-system/molecules/cards/card-project";
 
-import { EmptyState } from "@/shared/components/empty-state/empty-state";
+import { EmptyStateLite } from "@/shared/components/empty-state-lite/empty-state-lite";
 import { ErrorState } from "@/shared/components/error-state/error-state";
 import { ShowMore } from "@/shared/components/show-more/show-more";
 import { SidePanelBody } from "@/shared/features/side-panels/side-panel-body/side-panel-body";
@@ -14,46 +21,76 @@ import { useProgramSelection } from "@/shared/panels/_flows/ungrant-flow/_panels
 import { useUngrantFlow } from "@/shared/panels/_flows/ungrant-flow/ungrant-flow.context";
 
 function Programs() {
+  const { t } = useTranslation();
   const { projectId, selectProgram } = useUngrantFlow();
+  const [search, setSearch] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState(search);
+
+  useDebounce(
+    () => {
+      setDebouncedSearch(search);
+    },
+    300,
+    [search]
+  );
 
   const { data, isLoading, isError, hasNextPage, fetchNextPage, isFetchingNextPage } =
     ProjectReactQueryAdapter.client.useGetProjectPrograms({
       pathParams: { projectId },
+      queryParams: {
+        search: debouncedSearch,
+      },
       options: {
         enabled: Boolean(projectId),
       },
     });
 
-  if (isLoading) {
-    return (
-      <div className={"grid gap-lg"}>
-        <CardProjectLoading />
-        <CardProjectLoading />
-        <CardProjectLoading />
-      </div>
-    );
-  }
-
-  if (isError) {
-    return <ErrorState />;
-  }
-
-  if (!data) {
-    return <EmptyState titleTranslate={{ token: "panels:ungrantProgramSelection.empty.title" }} />;
-  }
-
-  const flatPrograms = data.pages.flatMap(page => page.programs);
+  const flatPrograms = useMemo(() => data?.pages.flatMap(page => page.programs) ?? [], [data]);
 
   function handleProgramClick(program: ProjectProgramListItemInterface) {
     selectProgram(program);
   }
 
+  function renderPrograms() {
+    if (isLoading) {
+      return (
+        <div className={"grid gap-lg"}>
+          <CardProjectLoading />
+          <CardProjectLoading />
+          <CardProjectLoading />
+        </div>
+      );
+    }
+
+    if (isError) {
+      return <ErrorState />;
+    }
+
+    if (!flatPrograms.length) {
+      return <EmptyStateLite />;
+    }
+    return (
+      <>
+        {flatPrograms.map(program => (
+          <CardProgram key={program.id} program={program} onClick={handleProgramClick} />
+        ))}
+        {hasNextPage ? <ShowMore onNext={fetchNextPage} loading={isFetchingNextPage} /> : null}
+      </>
+    );
+  }
+
   return (
     <div className={"grid gap-lg"}>
-      {flatPrograms.map(program => (
-        <CardProgram key={program.id} program={program} onClick={handleProgramClick} />
-      ))}
-      {hasNextPage ? <ShowMore onNext={fetchNextPage} loading={isFetchingNextPage} /> : null}
+      <Input
+        name="searchPrograms"
+        value={search}
+        size="sm"
+        onChange={e => setSearch(e.target.value)}
+        startContent={<Icon component={Search} classNames={{ base: "text-foreground-tertiary" }} />}
+        placeholder={t("panels:ungrantProgramSelection.search.placeholder")}
+      />
+
+      {renderPrograms()}
     </div>
   );
 }
