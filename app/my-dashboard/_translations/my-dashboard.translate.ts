3ee35b812696d39974f2bbf45dd925d467108728
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-12-04 16:46:22 +0100

    refactor: removed old rewards sidepanel

--- app/my-dashboard/_translations/my-dashboard.translate.ts
+++ app/my-dashboard/_translations/my-dashboard.translate.ts
@@ -1,6 +1,5 @@
 import enBudgetAvailable from "@/app/my-dashboard/financial/_features/budget-available-cards/budget-available.en.json";
 import enFinancialColumnChart from "@/app/my-dashboard/financial/_features/financial-column-chart/financial-column-chart.en.json";
-import enTransactionPanel from "@/app/my-dashboard/financial/_features/transactions-sidepanel/_translations/transaction-sidepanel.en.json";
 
 import enMyDashboard from "../_translations/my-dashboard.en.json";
 
@@ -9,6 +8,5 @@ export const enMyDashboardTranslation = {
     detail: enMyDashboard,
     budgetAvailable: enBudgetAvailable,
     financialColumnChart: enFinancialColumnChart,
-    transactionPanel: enTransactionPanel,
   },
 };
