fc3c9827110f3d27c7524f80296ee8c10c2142ae
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-12-06 16:47:46 +0100

    skeleton

--- design-system/molecules/cards/card-project-marketplace/card-project-marketplace.loading.tsx
+++ design-system/molecules/cards/card-project-marketplace/card-project-marketplace.loading.tsx
@@ -1,13 +1,5 @@
 import { Skeleton } from "@/design-system/atoms/skeleton";
 
 export function CardProjectMarketplaceLoading() {
-  return (
-    <Skeleton
-      style={{
-        width: "100%",
-        // TODO: Add the correct height
-        height: 100,
-      }}
-    />
-  );
+  return <Skeleton className="h-[294px] w-full" />;
 }
