a3dcea99fd747feda607516b814f6896f95287eb
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-12-06 18:13:41 +0100

    wip: trending projects

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -3,18 +3,27 @@
 import { Typo } from "@/design-system/atoms/typo";
 
 import { ProjectCategoryList } from "./_features/project-category-list/project-category-list";
+import { TrendingProjects } from "./_features/trending-projects/trending-projects";
 
 export default function ExplorePage() {
   return (
     <div className="mx-auto flex max-w-laptop flex-col gap-6xl py-4xl">
-      <Typo variant="heading" size="xl" translate={{ token: "explore:title" }} />
-      <div className="flex flex-col gap-lg">
+      <section className="flex flex-col gap-lg">
+        <div className="flex flex-col gap-md">
+          <Typo variant="heading" size="xs" weight="medium" translate={{ token: "explore:trending.title" }} />
+          <Typo color="secondary" size="xs" translate={{ token: "explore:trending.description" }} />
+        </div>
+
+        <TrendingProjects />
+      </section>
+
+      <section className="flex flex-col gap-lg">
         <div className="flex flex-col gap-md">
           <Typo variant="heading" size="xs" weight="medium" translate={{ token: "explore:expertise.title" }} />
           <Typo color="secondary" size="xs" translate={{ token: "explore:expertise.description" }} />
         </div>
         <ProjectCategoryList />
-      </div>
+      </section>
     </div>
   );
 }
