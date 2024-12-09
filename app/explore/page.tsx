9d58f4c7db94badc23a0b1fc1ef190946f88e1b1
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2024-12-09 10:33:25 +0100

    e-2549-explore-trending-projects (#769)

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -2,19 +2,39 @@
 
 import { Typo } from "@/design-system/atoms/typo";
 
+import { ScrollView } from "@/shared/components/scroll-view/scroll-view";
+
 import { ProjectCategoryList } from "./_features/project-category-list/project-category-list";
+import { TrendingProjects } from "./_features/trending-projects/trending-projects";
 
 export default function ExplorePage() {
   return (
-    <div className="mx-auto flex max-w-laptop flex-col gap-6xl py-4xl">
-      <Typo variant="heading" size="xl" translate={{ token: "explore:title" }} />
-      <div className="flex flex-col gap-lg">
-        <div className="flex flex-col gap-md">
-          <Typo variant="heading" size="xs" weight="medium" translate={{ token: "explore:expertise.title" }} />
-          <Typo color="secondary" size="xs" translate={{ token: "explore:expertise.description" }} />
-        </div>
-        <ProjectCategoryList />
+    <ScrollView>
+      <div className="mx-auto flex max-w-laptop flex-col gap-6xl py-4xl">
+        <section className="flex flex-col gap-lg">
+          <div className="flex flex-col gap-md">
+            <div>
+              <Typo variant="heading" size="xs" weight="medium" translate={{ token: "explore:trending.title" }} />{" "}
+              <Typo variant="heading" size="xs" weight="medium" color="tertiary">
+                {/* TODO @hayden get the number of trending projects */}
+                (100)
+              </Typo>
+            </div>
+            <Typo color="secondary" size="xs" translate={{ token: "explore:trending.description" }} />
+          </div>
+
+          <TrendingProjects />
+        </section>
+
+        <section className="flex flex-col gap-lg">
+          <div className="flex flex-col gap-md">
+            <Typo variant="heading" size="xs" weight="medium" translate={{ token: "explore:expertise.title" }} />
+            <Typo color="secondary" size="xs" translate={{ token: "explore:expertise.description" }} />
+          </div>
+
+          <ProjectCategoryList />
+        </section>
       </div>
-    </div>
+    </ScrollView>
   );
 }
