ca8e214982d3a127f86dfb11c4f688b0cda666db
Alexis Benoliel <alexbenoliel@gmail.com>
2024-12-13 12:00:46 +0100

    Clean old components

--- shared/features/page-wrapper/page-wrapper.tsx
+++ shared/features/page-wrapper/page-wrapper.tsx
@@ -1,12 +1,10 @@
-import { SecondaryNavigation } from "@/shared/features/navigation/secondary-navigation/secondary-navigation";
 import { SidePanelsProvider } from "@/shared/features/side-panels/side-panels.context";
 
 import { PageWrapperProps } from "./page-wrapper.types";
 
-export function PageWrapper({ children, navigation }: PageWrapperProps) {
+export function PageWrapper({ children }: PageWrapperProps) {
   return (
     <div className={"flex size-full flex-col gap-md overflow-hidden"}>
-      {navigation ? <SecondaryNavigation {...navigation} /> : null}
       <SidePanelsProvider>{children}</SidePanelsProvider>
     </div>
   );
