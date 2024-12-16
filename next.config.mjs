196f8b7f84e2876b4fd8ddd263081586b432cafd
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-12-16 10:53:50 +0100

    E 2553 redirect user base on his role (#789)

--- next.config.mjs
+++ next.config.mjs
@@ -2,41 +2,36 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['lucide-react'],
+  transpilePackages: ["lucide-react"],
   async redirects() {
     return [
-      {
-        source: "/",
-        destination: "/programs",
-        permanent: false,
-      },
       // DATA
       {
-        source: '/data',
-        destination: '/data/overview',
+        source: "/data",
+        destination: "/data/overview",
         permanent: true,
       },
       // PROGRAMS
       {
-        source: '/programs/:programId',
-        destination: '/programs/:programId/projects',
+        source: "/programs/:programId",
+        destination: "/programs/:programId/projects",
         permanent: true,
       },
       // MANAGE PROJECTS
       {
-        source: '/manage-projects/:projectSlug',
-        destination: '/manage-projects/:projectSlug/contributors',
+        source: "/manage-projects/:projectSlug",
+        destination: "/manage-projects/:projectSlug/contributors",
         permanent: true,
       },
       // MY DASHBOARD
       {
-        source: '/my-dashboard',
-        destination: '/my-dashboard/contributions',
+        source: "/my-dashboard",
+        destination: "/my-dashboard/contributions",
         permanent: true,
       },
     ];
   },
-}
+};
 
 export default withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
