81a6e930b5f56ebce27e7aed4aa67019db0a62da
haydencleary <5160414+haydencleary@users.noreply.github.com>
2024-12-19 17:58:32 +0100

    Enhance Secondary Menu with Logout Functionality
    - Added a logout option to the Secondary Menu, including a new icon and translation support.
    - Integrated useLogout hook to handle user logout action.
    - Updated primary-navigation.en.json to include "logout" translation.

--- shared/features/navigation/_components/secondary-menu/secondary-menu.tsx
+++ shared/features/navigation/_components/secondary-menu/secondary-menu.tsx
@@ -1,13 +1,15 @@
-import { MessageSquare, Settings } from "lucide-react";
+import { LogIn, MessageSquare, Settings } from "lucide-react";
 
 import { ItemNav } from "@/design-system/molecules/item-nav";
 
 import { FeedbackDrawer } from "@/shared/features/feedback-drawer/feedback-drawer";
 import { useFeedbackDrawerState } from "@/shared/features/feedback-drawer/feedback-drawer.hooks";
+import { useLogout } from "@/shared/hooks/auth/use-logout";
 
 export function SecondaryMenu() {
   const feedbackDrawerState = useFeedbackDrawerState();
   const [, setIsOpen] = feedbackDrawerState;
+  const handleLogout = useLogout();
 
   function handleOpenFeedbackDrawer() {
     setIsOpen(true);
@@ -24,6 +26,11 @@ export function SecondaryMenu() {
         iconProps={{ component: Settings }}
         translate={{ token: "primaryNavigation:secondaryMenu.settings" }}
         isComingSoon={true}
+      />
+      <ItemNav
+        iconProps={{ component: LogIn }}
+        translate={{ token: "primaryNavigation:secondaryMenu.logout" }}
+        onClick={handleLogout}
       />
       <FeedbackDrawer state={feedbackDrawerState} />
     </>
