30c51369f3575b5f57c6927276b50790f19e4b6a
Alexis Benoliel <alexbeno@users.noreply.github.com>
2024-12-23 10:25:29 +0100

    add mock config (#832)

--- core/infrastructure/marketplace-api-client-adapter/config/base-url.ts
+++ core/infrastructure/marketplace-api-client-adapter/config/base-url.ts
@@ -1,1 +1,2 @@
 export const MARKETPLACE_API_BASE_URL = `https://${process.env.NEXT_PUBLIC_ONLYDUST_API_BASEPATH}`;
+export const MARKETPLACE_API_MOCK_URL = `http://${process.env.NEXT_PUBLIC_ONLYDUST_API_MOCK_BASEPATH}`;
