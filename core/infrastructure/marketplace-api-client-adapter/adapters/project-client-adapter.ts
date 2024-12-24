61c9f97e27767b4f2a63e2693dc620ee766531ef
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-12-24 17:22:43 +0100

    Enhance project contributors page and layout with new components and translations  - Added ContributorsTable component to the Project Contributors page for better display of contributor data. - Updated layout.tsx to improve styling and structure of the project overview section. - Introduced new translation keys for contributors in project-details.en.json. - Exported new query hook for fetching project contributors in the project client adapter. - Updated project contract types to include query parameters for contributors. - Enhanced Metric component to accept customizable icon sizes.  These changes improve the user experience by providing clearer contributor information and enhancing the overall layout.

--- core/infrastructure/marketplace-api-client-adapter/adapters/project-client-adapter.ts
+++ core/infrastructure/marketplace-api-client-adapter/adapters/project-client-adapter.ts
@@ -508,6 +508,7 @@ export class ProjectClientAdapter implements ProjectStoragePort {
         pathParams,
         queryParams,
         version: MarketplaceApiVersion.v2,
+        mock: true,
       });
 
       return {
