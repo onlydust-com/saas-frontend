3b454fa05d76e23e8c13a950f0c436bb7754db8a
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2024-12-27 17:19:26 +0100

    Implement address warning alert in rewards selection panel and add translations for warning messages

--- shared/panels/_flows/request-payment-flow/_panels/rewards-selection/rewards-selection.tsx
+++ shared/panels/_flows/request-payment-flow/_panels/rewards-selection/rewards-selection.tsx
@@ -1,7 +1,11 @@
+import { AlertTriangle, RefreshCw } from "lucide-react";
+import { useState } from "react";
+
 import { BillingProfileReactQueryAdapter } from "@/core/application/react-query-adapter/billing-profile";
 
 import { Button } from "@/design-system/atoms/button/variants/button-default";
 import { Skeleton } from "@/design-system/atoms/skeleton";
+import { Alert } from "@/design-system/molecules/alert";
 
 import { EmptyStateLite } from "@/shared/components/empty-state-lite/empty-state-lite";
 import { SidePanelBody } from "@/shared/features/side-panels/side-panel-body/side-panel-body";
@@ -12,13 +16,14 @@ import { BillingProfileAccordion } from "@/shared/panels/_flows/request-payment-
 import { RewardsCardsSelection } from "@/shared/panels/_flows/request-payment-flow/_panels/_components/rewards-cards-selection/rewards-cards-selection";
 import { useRewardsSelectionPanel } from "@/shared/panels/_flows/request-payment-flow/_panels/rewards-selection/rewards-selection.hooks";
 import { useRequestPaymentFlow } from "@/shared/panels/_flows/request-payment-flow/request-payment-flow.context";
+import { Translate } from "@/shared/translation/components/translate/translate";
 
 import { useAcceptInvoicingMandate } from "../accept-invoicing-mandate/accept-invoicing-mandate.hooks";
 import { useGenerateInvoice } from "../generate-invoice/generate-invoice.hooks";
 
 export function Content() {
   const { billingProfileId = "", rewardIds } = useRequestPaymentFlow();
-
+  const [isAddressWarningOpen, setIsAddressWarningOpen] = useState(true);
   const { open: openAcceptMandate } = useAcceptInvoicingMandate();
   const { open: openGenerateInvoice } = useGenerateInvoice();
 
@@ -97,9 +102,22 @@ export function Content() {
     );
   }
 
+  function handleOnClose() {
+    setIsAddressWarningOpen(false);
+  }
+
   return (
     <>
       <SidePanelBody>
+        {isAddressWarningOpen ? (
+          <Alert
+            color="warning"
+            title={<Translate token="panels:requestPaymentFlow.addressWarning.title" />}
+            description={<Translate token="panels:requestPaymentFlow.addressWarning.description" />}
+            icon={{ component: AlertTriangle }}
+            onClose={handleOnClose}
+          />
+        ) : null}
         <BillingProfileAccordion
           key={billingProfile.id}
           id={billingProfile.id}
