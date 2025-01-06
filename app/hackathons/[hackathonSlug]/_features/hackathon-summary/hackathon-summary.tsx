2ac4c4e1e0d26f76b92bdf16200869b3a8019602
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-01-06 15:19:26 +0100

    Enhance Hackathon Summary Component with Date Formatting and Layout Improvements
    - Introduced formatted date display for hackathon start and end dates, including times.
    - Updated layout to improve visual structure with flexbox for better alignment of date information.
    - Added an icon to enhance the user interface and provide a clearer separation between start and end date sections.
    - Refactored code for improved readability and maintainability.

--- app/hackathons/[hackathonSlug]/_features/hackathon-summary/hackathon-summary.tsx
+++ app/hackathons/[hackathonSlug]/_features/hackathon-summary/hackathon-summary.tsx
@@ -1,7 +1,10 @@
+import { ArrowRight } from "lucide-react";
+
 import { HackathonSummaryProps } from "@/app/hackathons/[hackathonSlug]/_features/hackathon-summary/hackathon-summary.types";
 
 import { HackathonReactQueryAdapter } from "@/core/application/react-query-adapter/hackathon";
 
+import { Icon } from "@/design-system/atoms/icon";
 import { PaperLoading } from "@/design-system/atoms/paper";
 import { Paper } from "@/design-system/atoms/paper/variants/paper-default";
 import { Typo } from "@/design-system/atoms/typo";
@@ -27,7 +30,7 @@ export function HackathonSummary({ hackathonSlug }: HackathonSummaryProps) {
 
   if (isError || !hackathon) return null;
 
-  console.log(hackathon);
+  const formattedDates = hackathon.formatDisplayDates();
 
   return (
     <Paper
@@ -53,7 +56,8 @@ export function HackathonSummary({ hackathonSlug }: HackathonSummaryProps) {
           classNames={{ title: "line-clamp-none" }}
         />
       </div>
-      <div className="p-lg">
+
+      <div className="flex flex-col gap-lg p-lg">
         <Typo
           variant="heading"
           size="xs"
@@ -62,7 +66,31 @@ export function HackathonSummary({ hackathonSlug }: HackathonSummaryProps) {
           classNames={{ base: "text-sm" }}
           translate={{ token: "hackathon:details.summary.date" }}
         />
+        <div className="flex items-center justify-between gap-lg">
+          <div className="flex flex-col">
+            <Typo variant="heading" size="xs">
+              {formattedDates.startDate}
+            </Typo>
+
+            <Typo weight="medium" size="xs" color="tertiary">
+              {formattedDates.startTime}
+            </Typo>
+          </div>
+
+          <Icon component={ArrowRight} color="purple" />
+
+          <div className="flex flex-col text-right">
+            <Typo variant="heading" size="xs">
+              {formattedDates.endDate}
+            </Typo>
+
+            <Typo weight="medium" size="xs" color="tertiary">
+              {formattedDates.endTime}
+            </Typo>
+          </div>
+        </div>
       </div>
+
       <div className="flex flex-col gap-lg p-lg">
         <Typo
           variant="heading"
@@ -76,6 +104,7 @@ export function HackathonSummary({ hackathonSlug }: HackathonSummaryProps) {
           {hackathon.location}
         </Typo>
       </div>
+
       <div className="p-lg">
         <Typo
           variant="heading"
