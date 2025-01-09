39c3767b3492baeb4b7fb6301c5fa4ab21754902
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-01-09 17:22:41 +0100

    Enhance Hackathon Community Page with Data Fetching and Navigation
    - Integrated `HackathonReactQueryAdapter` to fetch hackathon details by slug.
    - Replaced static content with dynamic breadcrumb navigation using `NavigationBreadcrumb`.
    - Added a placeholder paragraph for community description within a `ScrollView` component.
    - Improved overall structure for better user experience and data handling.

--- app/hackathons/[hackathonSlug]/community/page.tsx
+++ app/hackathons/[hackathonSlug]/community/page.tsx
@@ -1,10 +1,41 @@
 "use client";
 
-export default function HackathonCommunityPage() {
+import { HackathonReactQueryAdapter } from "@/core/application/react-query-adapter/hackathon";
+
+import { ScrollView } from "@/shared/components/scroll-view/scroll-view";
+import { NavigationBreadcrumb } from "@/shared/features/navigation/navigation.context";
+import { Translate } from "@/shared/translation/components/translate/translate";
+
+export default function HackathonCommunityPage({ params: { hackathonSlug } }: { params: { hackathonSlug: string } }) {
+  const { data: hackathon } = HackathonReactQueryAdapter.client.useGetHackathonBySlug({
+    pathParams: {
+      hackathonSlug,
+    },
+    options: {
+      enabled: Boolean(hackathonSlug),
+    },
+  });
+
   return (
-    <div className="p-lg">
-      <h1>Hackathon Community</h1>
-      {/* Add content here */}
-    </div>
+    <ScrollView>
+      <NavigationBreadcrumb
+        breadcrumb={[
+          {
+            id: "root",
+            label: hackathon?.title,
+          },
+          {
+            id: "community",
+            label: <Translate token={"hackathon:details.tabs.community"} />,
+          },
+        ]}
+      />
+
+      <p className="border-b-1 border-border-primary p-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eligendi dignissimos ullam odit, qui
+        officiis fuga non soluta mollitia, repudiandae, cupiditate blanditiis corporis facilis asperiores distinctio
+        commodi consequuntur quidem pariatur.
+      </p>
+    </ScrollView>
   );
 }
