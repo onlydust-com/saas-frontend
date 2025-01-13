368f8307faa2fa748055f7c4115236653bae4b80
Alexis Benoliel <alexbenoliel@gmail.com>
2025-01-13 15:39:57 +0100

    fix layout

--- app/ecosystems/[ecosystemSlug]/community/page.tsx
+++ app/ecosystems/[ecosystemSlug]/community/page.tsx
@@ -1,7 +1,13 @@
-export default function EcosystemCommunityPage() {
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+
+import { withClientOnly } from "@/shared/components/client-only/client-only";
+
+function EcosystemCommunityPage() {
   return (
     <div className="flex-1 p-lg">
       <h1>Community</h1>
     </div>
   );
 }
+
+export default withClientOnly(withAuthenticationRequired(EcosystemCommunityPage));
--- app/ecosystems/[ecosystemSlug]/overview/page.tsx
+++ app/ecosystems/[ecosystemSlug]/overview/page.tsx
@@ -1,7 +1,13 @@
-export default function EcosystemOverviewPage() {
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+
+import { withClientOnly } from "@/shared/components/client-only/client-only";
+
+function EcosystemOverviewPage() {
   return (
     <div className="flex-1 p-lg">
       <h1>Overview</h1>
     </div>
   );
 }
+
+export default withClientOnly(withAuthenticationRequired(EcosystemOverviewPage));
