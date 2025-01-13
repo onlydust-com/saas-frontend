c97c60b310d997701ab6c063509c01fb13e13263
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-01-13 14:59:38 +0100

    Refactor HackathonProjectListItemV2 to extend ProjectListItemV2 - Updated HackathonProjectListItemV2 class to inherit from ProjectListItemV2, streamlining the model structure. Removed redundant property definitions and utilized the superclass constructor for initialization, enhancing code maintainability and clarity.

--- core/domain/hackathon/models/hackathon-project-list-item-model-v2.ts
+++ core/domain/hackathon/models/hackathon-project-list-item-model-v2.ts
@@ -1,6 +1,7 @@
 import { components } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
 
-import { ProjectCategory, ProjectCategoryInterface } from "../../project-category/models/project-category-model";
+import { ProjectCategoryInterface } from "../../project-category/models/project-category-model";
+import { ProjectListItemV2 } from "../../project/models/project-list-item-model-v2";
 
 export type HackathonProjectListItemResponseV2 = components["schemas"]["ProjectShortResponseV2"];
 
@@ -8,25 +9,8 @@ export interface HackathonProjectListItemInterfaceV2 extends HackathonProjectLis
   categories: ProjectCategoryInterface[];
 }
 
-export class HackathonProjectListItemV2 implements HackathonProjectListItemInterfaceV2 {
-  id!: HackathonProjectListItemResponseV2["id"];
-  slug!: HackathonProjectListItemResponseV2["slug"];
-  name!: HackathonProjectListItemResponseV2["name"];
-  shortDescription!: HackathonProjectListItemResponseV2["shortDescription"];
-  contributorCount!: HackathonProjectListItemResponseV2["contributorCount"];
-  starCount!: HackathonProjectListItemResponseV2["starCount"];
-  forkCount!: HackathonProjectListItemResponseV2["forkCount"];
-  availableIssueCount!: HackathonProjectListItemResponseV2["availableIssueCount"];
-  goodFirstIssueCount!: HackathonProjectListItemResponseV2["goodFirstIssueCount"];
-  categories!: ProjectCategoryInterface[];
-  languages!: HackathonProjectListItemResponseV2["languages"];
-  logoUrl!: HackathonProjectListItemResponseV2["logoUrl"];
-  ecosystems!: HackathonProjectListItemResponseV2["ecosystems"];
-  odHackStats!: HackathonProjectListItemResponseV2["odHackStats"];
-  contributorStats!: HackathonProjectListItemResponseV2["contributorStats"];
-
+export class HackathonProjectListItemV2 extends ProjectListItemV2 implements HackathonProjectListItemInterfaceV2 {
   constructor(props: HackathonProjectListItemResponseV2) {
-    Object.assign(this, props);
-    this.categories = (props.categories || []).map(category => new ProjectCategory(category));
+    super(props);
   }
 }
