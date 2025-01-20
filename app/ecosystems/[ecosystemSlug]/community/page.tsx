f62122cd3fe58ff179c48a1fa6bdcfee03961936
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-01-20 18:06:44 +0100

    fix padding

--- app/ecosystems/[ecosystemSlug]/community/page.tsx
+++ app/ecosystems/[ecosystemSlug]/community/page.tsx
@@ -8,7 +8,7 @@ import { CommunityTable } from "./_components/community-table/community-table";
 
 function EcosystemCommunityPage({ params }: { params: { ecosystemSlug: string } }) {
   return (
-    <div className="pv-lg flex-1">
+    <div className="flex-1 px-lg">
       <CommunityTable ecosystemSlug={params.ecosystemSlug} />
     </div>
   );
