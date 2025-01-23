be391d552899b3b87b5bfea9c6905a5a50a52c1f
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-01-23 12:16:44 +0100

    feat: add Contributor storage ports to Bootstrap for client and server integration

---
+++ core/domain/contributor/models/contributor-rewards-distribution-model.ts
@@ -0,0 +1,16 @@
+import { components } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
+
+export type ContributorRewardsDistributionResponse = components["schemas"]["ContributorRewardsDistributionResponse"];
+
+export interface ContributorRewardsDistributionInterface extends ContributorRewardsDistributionResponse {}
+
+export class ContributorRewardsDistribution implements ContributorRewardsDistributionInterface {
+  totalRewardedUsdAmount!: ContributorRewardsDistributionResponse["totalRewardedUsdAmount"];
+  rewardedContributionCount!: ContributorRewardsDistributionResponse["rewardedContributionCount"];
+  averageRewardedUsdAmount!: ContributorRewardsDistributionResponse["averageRewardedUsdAmount"];
+  distribution!: ContributorRewardsDistributionResponse["distribution"];
+
+  constructor(props: ContributorRewardsDistributionResponse) {
+    Object.assign(this, props);
+  }
+}
---
+++ core/infrastructure/marketplace-api-client-adapter/adapters/contributor-client-adapter.ts
@@ -0,0 +1,173 @@
+import {
+  GetContributorContributionsOverTimeResponse,
+  GetContributorLastYearRewindResponse,
+  GetContributorLocDistributionResponse,
+  GetContributorProjectsResponse,
+  GetContributorRewardsDistributionResponse,
+  GetContributorStatsResponse,
+} from "@/core/domain/contributor/contributor-contract.types";
+import { ContributorContributionsOverTime } from "@/core/domain/contributor/models/contributor-contributions-over-time-model";
+import { ContributorLastYearRewind } from "@/core/domain/contributor/models/contributor-last-year-rewind-model";
+import { ContributorLocDistribution } from "@/core/domain/contributor/models/contributor-loc-distribution-model";
+import { ContributorProjectListItem } from "@/core/domain/contributor/models/contributor-project-list-item-model";
+import { ContributorRewardsDistribution } from "@/core/domain/contributor/models/contributor-rewards-distribution-model";
+import { ContributorStats } from "@/core/domain/contributor/models/contributor-stats-model";
+import { ContributorStoragePort } from "@/core/domain/contributor/outputs/contributor-storage-port";
+import { HttpClient } from "@/core/infrastructure/marketplace-api-client-adapter/http/http-client/http-client";
+import { FirstParameter } from "@/core/kernel/types";
+
+export class ContributorClientAdapter implements ContributorStoragePort {
+  constructor(private readonly client: HttpClient) {}
+
+  routes = {
+    getContributorStats: "contributors/:contributorId/stats",
+    getContributorRewardsDistribution: "contributors/:contributorId/rewards-distribution",
+    getContributorProjects: "contributors/:contributorId/projects",
+    getContributorLocDistribution: "contributors/:contributorId/loc-distribution",
+    getContributorLastYearRewind: "contributors/:contributorId/last-year-rewind",
+    getContributorContributionsOverTime: "contributors/:contributorId/contributions-over-time",
+  } as const;
+
+  getContributorStats = ({ pathParams }: FirstParameter<ContributorStoragePort["getContributorStats"]>) => {
+    const path = this.routes["getContributorStats"];
+    const method = "GET";
+    const tag = HttpClient.buildTag({ path, pathParams });
+
+    const request = async () => {
+      const data = await this.client.request<GetContributorStatsResponse>({
+        path,
+        method,
+        tag,
+        pathParams,
+      });
+
+      return new ContributorStats(data);
+    };
+
+    return {
+      request,
+      tag,
+    };
+  };
+
+  getContributorRewardsDistribution = ({
+    pathParams,
+  }: FirstParameter<ContributorStoragePort["getContributorRewardsDistribution"]>) => {
+    const path = this.routes["getContributorRewardsDistribution"];
+    const method = "GET";
+    const tag = HttpClient.buildTag({ path, pathParams });
+
+    const request = async () => {
+      const data = await this.client.request<GetContributorRewardsDistributionResponse>({
+        path,
+        method,
+        tag,
+        pathParams,
+      });
+
+      return new ContributorRewardsDistribution(data);
+    };
+
+    return {
+      request,
+      tag,
+    };
+  };
+
+  getContributorProjects = ({ pathParams }: FirstParameter<ContributorStoragePort["getContributorProjects"]>) => {
+    const path = this.routes["getContributorProjects"];
+    const method = "GET";
+    const tag = HttpClient.buildTag({ path, pathParams });
+
+    const request = async () => {
+      const data = await this.client.request<GetContributorProjectsResponse>({
+        path,
+        method,
+        tag,
+        pathParams,
+      });
+
+      return {
+        ...data,
+        projects: data.projects.map(project => new ContributorProjectListItem(project)),
+      };
+    };
+
+    return {
+      request,
+      tag,
+    };
+  };
+
+  getContributorLocDistribution = ({
+    pathParams,
+  }: FirstParameter<ContributorStoragePort["getContributorLocDistribution"]>) => {
+    const path = this.routes["getContributorLocDistribution"];
+    const method = "GET";
+    const tag = HttpClient.buildTag({ path, pathParams });
+
+    const request = async () => {
+      const data = await this.client.request<GetContributorLocDistributionResponse>({
+        path,
+        method,
+        tag,
+        pathParams,
+      });
+
+      return new ContributorLocDistribution(data);
+    };
+
+    return {
+      request,
+      tag,
+    };
+  };
+
+  getContributorLastYearRewind = ({
+    pathParams,
+  }: FirstParameter<ContributorStoragePort["getContributorLastYearRewind"]>) => {
+    const path = this.routes["getContributorLastYearRewind"];
+    const method = "GET";
+    const tag = HttpClient.buildTag({ path, pathParams });
+
+    const request = async () => {
+      const data = await this.client.request<GetContributorLastYearRewindResponse>({
+        path,
+        method,
+        tag,
+        pathParams,
+      });
+
+      return new ContributorLastYearRewind(data);
+    };
+
+    return {
+      request,
+      tag,
+    };
+  };
+
+  getContributorContributionsOverTime = ({
+    pathParams,
+  }: FirstParameter<ContributorStoragePort["getContributorContributionsOverTime"]>) => {
+    const path = this.routes["getContributorContributionsOverTime"];
+    const method = "GET";
+    const tag = HttpClient.buildTag({ path, pathParams });
+
+    const request = async () => {
+      const data = await this.client.request<GetContributorContributionsOverTimeResponse>({
+        path,
+        method,
+        tag,
+        pathParams,
+      });
+
+      return new ContributorContributionsOverTime(data);
+    };
+
+    return {
+      request,
+      tag,
+    };
+  };
+}
