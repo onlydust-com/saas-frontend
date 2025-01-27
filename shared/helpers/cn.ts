ef4893f3391e2fa880e876b4f609d86fa67fce6c
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-01-27 14:31:58 +0100

    Refactor: Update components configuration and deprecate cn helper function

--- shared/helpers/cn.ts
+++ shared/helpers/cn.ts
@@ -1,7 +1,7 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// DEPRECATED
+/** @deprecated */
 export function cn(...args: Array<ClassValue | undefined>) {
   return twMerge(clsx(args));
 }
