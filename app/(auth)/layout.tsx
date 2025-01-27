430125b91c35e1988f2f77d7b52afe1d0de02003
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-01-27 17:34:57 +0100

    Enhance auth layout with background and logo styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,23 @@
+import background from "@/public/images/backgrounds/auth-bg.png";
+import Image from "next/image";
 import { PropsWithChildren } from "react";
 
+import { Logo } from "@/shared/components/logo/logo";
+
 export default function AuthLayout({ children }: PropsWithChildren) {
-  return children;
+  return (
+    <>
+      <Image src={background} alt="" className={"absolute inset-0 size-full object-cover"} loading="eager" />
+
+      <div className="relative z-10 flex min-h-svh flex-col p-6 md:p-10">
+        <Logo classNames={{ image: "h-12" }} />
+
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <div className="flex w-full flex-col items-center justify-center">
+            <div className="w-full max-w-sm tablet:max-w-5xl">{children}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
