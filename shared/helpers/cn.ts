e914fdda17d5dfd594c72be2c654cc769005cacb
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-01-27 14:16:00 +0100

    Add shadcn/ui configuration and update styling dependencies

--- shared/helpers/cn.ts
+++ shared/helpers/cn.ts
@@ -1,6 +1,7 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// DEPRECATED
 export function cn(...args: Array<ClassValue | undefined>) {
   return twMerge(clsx(args));
 }
