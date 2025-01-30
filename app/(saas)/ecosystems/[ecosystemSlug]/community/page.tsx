7a7e5eb1e0eae343e2927706bc22fb7b3563d336
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-01-30 10:36:16 +0100

    Ecosystem : Add breadcrumb and fix scroll (#924)

--- app/(saas)/ecosystems/[ecosystemSlug]/community/page.tsx
+++ app/(saas)/ecosystems/[ecosystemSlug]/community/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 
 import { withClientOnly } from "@/shared/components/client-only/client-only";
+import { ScrollView } from "@/shared/components/scroll-view/scroll-view";
+import { NEXT_ROUTER } from "@/shared/constants/router";
+import { NavigationBreadcrumb } from "@/shared/features/navigation/navigation.context";
 import { withAuthenticated } from "@/shared/providers/auth-provider";
+import { Translate } from "@/shared/translation/components/translate/translate";
 
 import { CommunityTable } from "./_components/community-table/community-table";
 
 function EcosystemCommunityPage({ params }: { params: { ecosystemSlug: string } }) {
   return (
-    <div className="flex-1">
+    <ScrollView>
+      <NavigationBreadcrumb
+        breadcrumb={[
+          {
+            id: "root",
+            label: "Ecosystems",
+            href: NEXT_ROUTER.ecosystems.root,
+          },
+          {
+            id: "slug",
+            label: params.ecosystemSlug,
+          },
+          {
+            id: "contributors",
+            label: <Translate token={"ecosystems:details.tabs.community"} />,
+          },
+        ]}
+      />
       <CommunityTable ecosystemSlug={params.ecosystemSlug} />
-    </div>
+    </ScrollView>
   );
 }
 
