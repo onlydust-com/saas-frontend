c4e49367f93f0a81d630595a9e97006cd751bbd8
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-02-04 17:16:19 +0100

    update to never

--- core/application/react-query-adapter/me/client/use-get-update-my-github-profile.ts
+++ core/application/react-query-adapter/me/client/use-get-update-my-github-profile.ts
@@ -6,11 +6,10 @@ import {
 } from "@/core/application/react-query-adapter/helpers/use-query-adapter";
 import { bootstrap } from "@/core/bootstrap";
 import { MeFacadePort } from "@/core/domain/me/inputs/me-facade-port";
-import { MeInterface } from "@/core/domain/me/models/me-model";
 
 export function useGetUpdateGithubProfile({
   options,
-}: UseQueryFacadeParams<MeFacadePort["getUpdateGithubProfile"], MeInterface>) {
+}: UseQueryFacadeParams<MeFacadePort["getUpdateGithubProfile"], never>) {
   const meStoragePort = bootstrap.getMeStoragePortForClient();
 
   return useQuery(
