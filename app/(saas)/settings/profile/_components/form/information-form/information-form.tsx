693b6b6aad0c14110e78b3991e49ac9bd1e40b2d
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-02-04 10:46:07 +0100

    add image update form

--- app/(saas)/settings/profile/_components/form/information-form/information-form.tsx
+++ app/(saas)/settings/profile/_components/form/information-form/information-form.tsx
@@ -5,8 +5,9 @@ import { z } from "zod";
 
 import { MeReactQueryAdapter } from "@/core/application/react-query-adapter/me";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shared/ui/form";
+import { ImageInput } from "@/design-system/molecules/image-input";
+
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/shared/ui/form";
 import { Input } from "@/shared/ui/input";
 
 import { formSchema } from "../form.types";
@@ -44,30 +45,20 @@ export function InformationForm() {
           <FormField
             control={form.control}
             name="avatarUrl"
-            render={({ field: { value, onChange, ...field } }) => (
+            render={({ field: { onChange, name } }) => (
               <FormItem>
                 <FormLabel>Profile Picture</FormLabel>
+                <FormDescription>SVG, PNG, JPG or GIF (MAX. 400x400px).</FormDescription>
                 <FormControl>
                   <div className="flex items-center gap-2">
-                    <Avatar className="size-12 rounded-xl">
-                      {value ? (
-                        <AvatarImage src={value} alt="Profile" />
-                      ) : (
-                        <AvatarFallback className="rounded-xl">{data?.login.charAt(0)}</AvatarFallback>
-                      )}
-                    </Avatar>
-                    <Input
-                      type="file"
-                      accept="image/*"
-                      onChange={e => {
-                        const file = e.target.files?.[0];
-
-                        if (file) {
-                          console.log(file);
-                          uploadProfilePicture(file);
-                        }
+                    <ImageInput
+                      name={name}
+                      value={data?.avatarUrl}
+                      onChange={onChange}
+                      buttonProps={{
+                        children: "Update",
+                        classNames: { base: "w-16" },
                       }}
-                      {...field}
                     />
                   </div>
                 </FormControl>
