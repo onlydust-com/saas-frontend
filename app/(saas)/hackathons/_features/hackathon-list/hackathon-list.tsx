30d4b971f92b6e3a9a3f4ac3d35092432f45d672
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-02-05 10:40:39 +0100

    Refactor HackathonList component formatting

--- app/(saas)/hackathons/_features/hackathon-list/hackathon-list.tsx
+++ app/(saas)/hackathons/_features/hackathon-list/hackathon-list.tsx
@@ -70,9 +70,7 @@ export function HackathonList() {
 
       {renderUpcomingHackathon()}
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {renderClosedHackathons()}
-      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">{renderClosedHackathons()}</div>
     </div>
   );
 }
