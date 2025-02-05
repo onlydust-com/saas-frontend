74dc0d946e5b9400fd20899bfbbe2c09aa3e92c1
Anthony Buisset <abuisset@gmail.com>
2025-02-05 14:42:36 +0100

    add shadcn accordion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,23 +98,45 @@ const config: Config = withTV({
   			}
   		},
   		screens: {
-  			mobile: `${BREAKPOINTS.mobile}px`,
-  			tablet: `${BREAKPOINTS.tablet}px`,
-  			laptop: `${BREAKPOINTS.laptop}px`,
-  			desktop: `${BREAKPOINTS.desktop}px`,
-  			wide: `${BREAKPOINTS.wide}px`
+  			mobile: '`${BREAKPOINTS.mobile}px`',
+  			tablet: '`${BREAKPOINTS.tablet}px`',
+  			laptop: '`${BREAKPOINTS.laptop}px`',
+  			desktop: '`${BREAKPOINTS.desktop}px`',
+  			wide: '`${BREAKPOINTS.wide}px`'
   		},
   		maxWidth: {
-  			mobile: `${BREAKPOINTS.mobile}px`,
-  			tablet: `${BREAKPOINTS.tablet}px`,
-  			laptop: `${BREAKPOINTS.laptop}px`,
-  			desktop: `${BREAKPOINTS.desktop}px`,
-  			wide: `${BREAKPOINTS.wide}px`
+  			mobile: '`${BREAKPOINTS.mobile}px`',
+  			tablet: '`${BREAKPOINTS.tablet}px`',
+  			laptop: '`${BREAKPOINTS.laptop}px`',
+  			desktop: '`${BREAKPOINTS.desktop}px`',
+  			wide: '`${BREAKPOINTS.wide}px`'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
