71be1d95ff5483bb22f24c9f8613146752c4003a
Anthony Buisset <abuisset@gmail.com>
2025-02-06 09:03:58 +0100

    fix typography

--- app/(saas)/create-project/ProjectCreation/views/GithubRepository/components/GithubRepositoryCount.tsx
+++ app/(saas)/create-project/ProjectCreation/views/GithubRepository/components/GithubRepositoryCount.tsx
@@ -1,11 +1,11 @@
-import { TypographyP } from "@/shared/ui/typography";
+import { TypographySmall } from "@/shared/ui/typography";
 import { CopyCheck } from "lucide-react";
 
 export const GithubRepositoryCount = ({ selected, total }: { selected: number; total: number }) => {
   return (
     <div className="flex h-auto items-center justify-start gap-1 rounded-full border px-3 py-1">
       <CopyCheck />
-      <TypographyP>{`${selected}/${total} repos selected`}</TypographyP>
+      <TypographySmall>{`${selected}/${total} repos selected`}</TypographySmall>
     </div>
   );
 };
