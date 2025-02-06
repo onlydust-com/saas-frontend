d3b56ee34234d81cc10b975e4127241c7a55fb7e
Anthony Buisset <abuisset@gmail.com>
2025-02-06 09:02:50 +0100

    pass form as parameter

--- shared/panels/project-update-sidepanel/_components/global-information/global-information.types.ts
+++ shared/panels/project-update-sidepanel/_components/global-information/global-information.types.ts
@@ -1,5 +1,8 @@
 import { ProjectInterface } from "@/core/domain/project/models/project-model";
+import { UseFormReturn } from "react-hook-form";
+import { EditProjectFormData } from "../../project-update-sidepanel.types";
 
 export interface GlobalInformationProps {
   project: ProjectInterface;
+  form: UseFormReturn<EditProjectFormData, unknown>;
 }
