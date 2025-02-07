5d81406ca7eaa6b36516ec2b3396f9b76d53faaa
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2025-02-07 10:11:16 +0100

    B 2985 add notifications tab (#960)

--- core/domain/me/me-constants.ts
+++ core/domain/me/me-constants.ts
@@ -1,3 +1,5 @@
+import { components } from "@/core/infrastructure/marketplace-api-client-adapter/__generated/api";
+
 export const LOCAL_STORAGE_JOINING_REASON_KEY = "joiningReason";
 
 export enum USER_PROFILE_JOINING_REASON {
@@ -5,3 +7,25 @@ export enum USER_PROFILE_JOINING_REASON {
   MAINTAINER = "MAINTAINER",
   SPONSOR = "SPONSOR",
 }
+
+type categories = components["schemas"]["NotificationSettingResponse"]["category"];
+
+export const MeNotificationSettingsCategory: { [key in categories]: key } = {
+  MAINTAINER_PROJECT_CONTRIBUTOR: "MAINTAINER_PROJECT_CONTRIBUTOR",
+  MAINTAINER_PROJECT_PROGRAM: "MAINTAINER_PROJECT_PROGRAM",
+  CONTRIBUTOR_REWARD: "CONTRIBUTOR_REWARD",
+  CONTRIBUTOR_PROJECT: "CONTRIBUTOR_PROJECT",
+  GLOBAL_BILLING_PROFILE: "GLOBAL_BILLING_PROFILE",
+  GLOBAL_MARKETING: "GLOBAL_MARKETING",
+  PROGRAM_LEAD: "PROGRAM_LEAD",
+  SPONSOR_LEAD: "SPONSOR_LEAD",
+  CONTRIBUTOR_REWIND: "CONTRIBUTOR_REWIND",
+} as const;
+
+type channels = components["schemas"]["NotificationSettingResponse"]["channels"][0];
+
+export const MeNotificationSettingsChannel: { [key in channels]: key } = {
+  EMAIL: "EMAIL",
+  SUMMARY_EMAIL: "SUMMARY_EMAIL",
+  IN_APP: "IN_APP",
+} as const;
