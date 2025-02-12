dc746473bf8ccb09b2917e9f4451d91af810d7d2
Anthony Buisset <abuisset@gmail.com>
2025-02-12 17:47:48 +0100

    fix build

--- app/_assets/img/index.ts
+++ app/_assets/img/index.ts
@@ -1,6 +1,5 @@
 import githubTutorialStep2 from "./github_tutorial_step_2.webp";
 import githubTutorialStep3Gif from "./github_tutorial_step_3.gif";
-import odSayAvatar from "./od_say_avatar.png";
 
 export const IMAGES = {
   github: {
@@ -8,8 +7,5 @@ export const IMAGES = {
       step2: githubTutorialStep2.src,
       step3: githubTutorialStep3Gif.src,
     },
-  },
-  odSay: {
-    avatar: odSayAvatar.src,
   },
 };
