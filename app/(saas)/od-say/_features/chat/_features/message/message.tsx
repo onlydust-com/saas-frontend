458bb5f16f50c497080596f1c8c623dda2240eda
Anthony Buisset <abuisset@gmail.com>
2025-02-13 22:40:27 +0100

    modify project cards

--- app/(saas)/od-say/_features/chat/_features/message/message.tsx
+++ app/(saas)/od-say/_features/chat/_features/message/message.tsx
@@ -3,17 +3,17 @@ import { useCallback, useMemo } from "react";
 import { ContributionReactQueryAdapter } from "@/core/application/react-query-adapter/contribution";
 import { ProjectReactQueryAdapter } from "@/core/application/react-query-adapter/project";
 import { ContributionAs } from "@/core/domain/contribution/models/contribution.types";
+import { ProjectListItemInterfaceV2 } from "@/core/domain/project/models/project-list-item-model-v2";
 
-import {
-  CardProjectMarketplace,
-  CardProjectMarketplaceLoading,
-} from "@/design-system/molecules/cards/card-project-marketplace";
+import { CardProjectMarketplaceLoading } from "@/design-system/molecules/cards/card-project-marketplace";
 
 import { ErrorState } from "@/shared/components/error-state/error-state";
 import { CardContributionKanban } from "@/shared/features/card-contribution-kanban/card-contribution-kanban";
 import { Markdown } from "@/shared/features/markdown/markdown";
 import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
-import { Button } from "@/shared/ui/button";
+import { Badge } from "@/shared/ui/badge";
+import { Card } from "@/shared/ui/card";
+import { TypographyMuted, TypographySmall } from "@/shared/ui/typography";
 
 import { MessageProps } from "./message.types";
 
@@ -23,6 +23,45 @@ const Thinking = () => (
     <span className="animate-pulse delay-300">.</span>
     <span className="animate-pulse delay-500">.</span>
   </div>
+);
+
+const ProjectCard = ({
+  name,
+  logoUrl,
+  shortDescription,
+  categories,
+  languages,
+  ...props
+}: ProjectListItemInterfaceV2 & React.HTMLAttributes<HTMLDivElement>) => (
+  <Card className="flex flex-col gap-2 p-2 hover:cursor-pointer hover:bg-background-primary-hover" {...props}>
+    <div className="flex flex-row gap-2">
+      <Avatar>
+        <AvatarImage src={logoUrl} />
+        <AvatarFallback>{name}</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col gap-1">
+        <TypographySmall>{name}</TypographySmall>
+        <TypographyMuted>{shortDescription}</TypographyMuted>
+      </div>
+    </div>
+    <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-row items-center gap-1">
+        {categories.map(category => (
+          <Badge key={category.name} variant="outline">
+            {category.name}
+          </Badge>
+        ))}
+      </div>
+      <div className="flex flex-row items-center gap-1">
+        {languages.map(language => (
+          <Avatar className="size-5" key={language.name}>
+            <AvatarImage src={language.logoUrl} />
+            <AvatarFallback>{language.name}</AvatarFallback>
+          </Avatar>
+        ))}{" "}
+      </div>
+    </div>
+  </Card>
 );
 
 export default function Message({
@@ -82,29 +121,9 @@ export default function Message({
     }
 
     return (
-      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+      <div className="flex flex-col gap-2">
         {projects.map(project => (
-          <CardProjectMarketplace
-            as={Button}
-            htmlProps={{
-              onClick: () => onOpenProject(project.id),
-            }}
-            classNames={{ base: "bg-background hover:bg-background-secondary-hover" }}
-            key={project.id}
-            name={project.name}
-            slug={project.slug}
-            description={project.shortDescription}
-            logoUrl={project.logoUrl}
-            contributorCount={project.contributorCount}
-            starCount={project.starCount}
-            forkCount={project.forkCount}
-            availableIssueCount={project.availableIssueCount}
-            goodFirstIssueCount={project.goodFirstIssueCount}
-            categories={project.categories}
-            languages={project.languages}
-            ecosystems={project.ecosystems}
-            tags={project.tags}
-          />
+          <ProjectCard {...project} key={project.id} onClick={() => onOpenProject(project.id)} />
         ))}
       </div>
     );
