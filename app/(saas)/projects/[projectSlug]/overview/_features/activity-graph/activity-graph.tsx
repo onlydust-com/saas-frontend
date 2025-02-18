d9275122c872e0c9f3e77ded66ad045600a3a0f5
Alexis Benoliel <alexbeno@users.noreply.github.com>
2025-02-18 18:47:57 +0100

    Remove mock on activity (#1000)

--- app/(saas)/projects/[projectSlug]/overview/_features/activity-graph/activity-graph.tsx
+++ app/(saas)/projects/[projectSlug]/overview/_features/activity-graph/activity-graph.tsx
@@ -1,22 +1,30 @@
+import { setDay, setWeek, setYear } from "date-fns";
+import { useMemo } from "react";
+
+import { ProjectReactQueryAdapter } from "@/core/application/react-query-adapter/project";
+
 import { ActivityGraph as ActivityGraphComponent } from "@/shared/features/contributors/activity-graph/activity-graph";
 import { Card } from "@/shared/ui/card";
 import { TypographyH3 } from "@/shared/ui/typography";
 
-export function ActivityGraph() {
-  const mockActivityData = Array.from({ length: 365 }, (_, i) => {
-    const date = new Date();
-    date.setDate(date.getDate() - (364 - i));
-    return {
-      date,
-      count: Math.floor(Math.random() * 10), // Random number of contributions (0-9)
-      hasReward: Math.random() > 0.9, // 10% chance of having a reward
-    };
+export function ActivityGraph({ projectIdOrSlug }: { projectIdOrSlug: string }) {
+  const { data } = ProjectReactQueryAdapter.client.useGetProjectActivityBySlugOrId({
+    pathParams: { projectIdOrSlug },
   });
+
+  const formated = useMemo(() => {
+    return data?.days.map(day => ({
+      ...day,
+      date: setYear(setWeek(setDay(new Date(), day.day), day.week), day.year),
+      count: (day.rewardCount || 0) + (day.codeReviewCount || 0) + (day.issueCount || 0) + (day.pullRequestCount || 0),
+      hasReward: day.rewardCount > 0,
+    }));
+  }, [data]);
 
   return (
     <Card className={"relative flex w-full flex-col gap-4 overflow-hidden p-4"}>
       <TypographyH3>Github Activity</TypographyH3>
-      <ActivityGraphComponent data={mockActivityData} />
+      <ActivityGraphComponent data={formated} />
     </Card>
   );
 }
