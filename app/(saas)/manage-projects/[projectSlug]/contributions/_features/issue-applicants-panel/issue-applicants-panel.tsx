da16cc14ae4077baf724249bcf9cbeabfc884c41
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2025-02-19 18:06:10 +0100

    one-63-applicant-list-card-applicant (#1010)

--- app/(saas)/manage-projects/[projectSlug]/contributions/_features/issue-applicants-panel/issue-applicants-panel.tsx
+++ app/(saas)/manage-projects/[projectSlug]/contributions/_features/issue-applicants-panel/issue-applicants-panel.tsx
@@ -1,13 +1,17 @@
 import { PropsWithChildren, useCallback, useState } from "react";
 
+import { ApplicantCard } from "@/app/(saas)/manage-projects/[projectSlug]/contributions/_components/applicant-card/applicant-card";
+
 import { ContributionReactQueryAdapter } from "@/core/application/react-query-adapter/contribution";
+import { IssueReactQueryAdapter } from "@/core/application/react-query-adapter/issue";
 
 import { ContributionBadge } from "@/design-system/molecules/contribution-badge/variants/contribution-badge-default";
 
 import { Button } from "@/shared/ui/button";
 import { ScrollArea } from "@/shared/ui/scroll-area";
-import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/shared/ui/sheet";
-import { TypographyH3 } from "@/shared/ui/typography";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTrigger } from "@/shared/ui/sheet";
+import { Skeleton } from "@/shared/ui/skeleton";
+import { TypographyH4, TypographyMuted } from "@/shared/ui/typography";
 
 export function IssueApplicantsPanel({ children, id }: PropsWithChildren<{ id: string }>) {
   const [open, setOpen] = useState(false);
@@ -19,40 +23,70 @@ export function IssueApplicantsPanel({ children, id }: PropsWithChildren<{ id: s
     },
   });
 
-  const renderContent = useCallback(() => {
+  const {
+    data: dataApplicants,
+    isLoading: isLoadingApplicants,
+    isError: isErrorApplicants,
+  } = IssueReactQueryAdapter.client.useGetIssueApplicants({
+    pathParams: { contributionUuid: id },
+    queryParams: {
+      isIgnored: false,
+    },
+    options: {
+      enabled: Boolean(id) && open,
+    },
+  });
+
+  const applicants = dataApplicants?.pages.flatMap(page => page.applicants) ?? [];
+
+  const renderHeader = useCallback(() => {
     if (!contribution) return null;
 
     return (
+      <SheetHeader className={"flex w-full flex-row items-center justify-start gap-lg space-y-0"}>
+        <ContributionBadge
+          type={contribution.type}
+          number={contribution?.githubNumber}
+          githubStatus={contribution?.githubStatus}
+        />
+
+        <TypographyH4 className="line-clamp-1">{contribution?.githubTitle}</TypographyH4>
+      </SheetHeader>
+    );
+  }, [contribution]);
+
+  const renderApplications = useCallback(() => {
+    if (isLoadingApplicants)
+      return Array.from({ length: 3 }).map((_, index) => <Skeleton key={index} className="h-24" />);
+
+    if (isErrorApplicants)
+      return (
+        <div className="py-10 text-center">
+          <TypographyMuted>No applicants found</TypographyMuted>
+        </div>
+      );
+
+    if (!applicants) return null;
+
+    return applicants.map(applicant => <ApplicantCard key={applicant.applicationId} applicant={applicant} />);
+  }, [applicants, isLoadingApplicants, isErrorApplicants]);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger className="w-full">{children}</SheetTrigger>
+
       <SheetContent className="flex flex-col">
-        <SheetHeader>
-          <SheetTitle>
-            <div className={"flex w-full flex-row items-center justify-start gap-lg overflow-hidden"}>
-              <ContributionBadge
-                type={contribution.type}
-                number={contribution.githubNumber}
-                githubStatus={contribution.githubStatus}
-              />
+        {renderHeader()}
 
-              <TypographyH3 className="line-clamp-1">{contribution.githubTitle}</TypographyH3>
-            </div>
-          </SheetTitle>
-        </SheetHeader>
-
-        <ScrollArea className="flex-1">Content</ScrollArea>
+        <ScrollArea className="flex-1">
+          <div className="flex flex-col gap-2">{renderApplications()}</div>
+        </ScrollArea>
 
         <SheetFooter className="justify-end">
           <Button variant="outline">Scoring</Button>
           <Button variant="outline">See issue</Button>
         </SheetFooter>
       </SheetContent>
-    );
-  }, [contribution]);
-
-  return (
-    <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger className="w-full">{children}</SheetTrigger>
-
-      {renderContent()}
     </Sheet>
   );
 }
