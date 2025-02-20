56f0dbfa1039b35b028474a7d4e5feb16bf8fbae
Hayden Cleary <5160414+haydencleary@users.noreply.github.com>
2025-02-20 14:53:17 +0100

    Enhance apply issue sidepanel form handling and disable textarea for … (#1018)

--- shared/panels/apply-issue-sidepanel/_components/apply/apply.tsx
+++ shared/panels/apply-issue-sidepanel/_components/apply/apply.tsx
@@ -7,13 +7,17 @@ import { Typo } from "@/design-system/atoms/typo";
 
 import { ApplyIssueSidepanelForm } from "../../apply-issue-sidepanel.types";
 
-export function Apply() {
+export function Apply({ hasCurrentUserApplication }: { hasCurrentUserApplication: boolean }) {
   const { control } = useFormContext<ApplyIssueSidepanelForm>();
 
   return (
     <div className="flex w-full flex-col gap-4">
       <Typo size={"md"} weight={"medium"} translate={{ token: "panels:applyIssue.apply.githubComment" }} />
-      <Controller control={control} name="githubComment" render={({ field }) => <Textarea {...field} />} />
+      <Controller
+        control={control}
+        name="githubComment"
+        render={({ field }) => <Textarea {...field} isDisabled={hasCurrentUserApplication} />}
+      />
       <div className="flex flex-row items-center justify-start gap-1">
         <Icon component={Info} size={"md"} classNames={{ base: "text-typography-tertiary" }} />
         <Typo size={"xs"} weight={"medium"} translate={{ token: "panels:applyIssue.apply.info" }} color="tertiary" />
