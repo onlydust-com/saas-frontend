5d06608ea8c9b403b9fec3d8c79d8801da056670
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-02-20 15:00:17 +0100

    Fix odhack

--- shared/panels/apply-issue-sidepanel/apply-issue-sidepanel.tsx
+++ shared/panels/apply-issue-sidepanel/apply-issue-sidepanel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { toast } from "sonner";
 
@@ -212,23 +212,7 @@ function Content() {
 
   const { data: user } = MeReactQueryAdapter.client.useGetMe({});
 
-  const { data: pendingContributionsData } = ContributionReactQueryAdapter.client.useGetContributions({
-    queryParams: {
-      applicantIds: user?.githubUserId ? [user.githubUserId] : [],
-    },
-    options: {
-      enabled: !!user?.githubUserId,
-    },
-  });
-
-  const pendingContributions = useMemo(
-    () => pendingContributionsData?.pages.flatMap(page => page.contributions) ?? [],
-    [pendingContributionsData]
-  );
-
-  const currentUserApplication =
-    user?.pendingApplications?.find(application => application.issue?.id === issue?.id) ||
-    pendingContributions?.find(contribution => issueFromContribution(contribution).id === issue?.id);
+  const currentUserApplication = user?.pendingApplications?.find(application => application.issue?.id === issue?.id);
 
   const hasCurrentUserApplication = !!currentUserApplication;
 
