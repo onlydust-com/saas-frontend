6e44a10b0afe48fcccea64f85d001300411f7e00
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-02-21 11:51:49 +0100

    uypdate page

--- app/(saas)/quests/[questId]/applications/[applicationId]/page.tsx
+++ app/(saas)/quests/[questId]/applications/[applicationId]/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import { Description } from "@/app/(saas)/projects/[projectSlug]/overview/_features/description/description";
-
 import { PageContainer } from "@/shared/features/page/page-container/page-container";
 
 import { ActivityGraph } from "./_features/activity-graph/activity-graph";
 import { DevCareNote } from "./_features/dev-care-note/dev-care-note";
+import { FilloutResponse } from "./_features/fillout-response/fillout-response";
 import { Languages } from "./_features/languages/languages";
 import { PageHeader } from "./_features/page-header/page-header";
 import { RecentActivity } from "./_features/recent-activity/recent-activity";
 import { Projects } from "./_features/similar-projects/projects";
-
-
-
-
-import { FilloutResponse } from "./_features/fillout-response/fillout-response";
 
 export default function QuestApplicationPage({ params }: { params: { applicationId: string } }) {
   // 1, contributor detail + metrics
@@ -25,12 +19,11 @@ export default function QuestApplicationPage({ params }: { params: { application
   const githubId = 17259618;
   const note =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
-  "use client";
 
   return (
     <PageContainer size="small" className="flex-1">
       <PageHeader githubLogin={login} />
-      <FilloutResponse applicationId={params.applicationId} />
+
       <div className="grid w-full grid-cols-1 gap-6 overflow-hidden pt-6 lg:grid-cols-4">
         <div className="grid lg:col-span-1">
           <Languages githubLogin={login} />
@@ -41,6 +34,10 @@ export default function QuestApplicationPage({ params }: { params: { application
 
         <div className="col-span-full">
           <DevCareNote>{note}</DevCareNote>
+        </div>
+
+        <div className="col-span-full">
+          <FilloutResponse applicationId={params.applicationId} />
         </div>
 
         <div className="grid lg:col-span-2">
