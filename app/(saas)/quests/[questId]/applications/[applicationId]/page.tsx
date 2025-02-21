24699554c1dbd2947005c9e52da4f5abf1f78184
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-02-21 10:41:38 +0100

    add page header

---
+++ app/(saas)/quests/[questId]/applications/[applicationId]/page.tsx
@@ -0,0 +1,8 @@
+export function QuestApplicationPage() {
+  // 1, contributor detail + metrics
+  // 2, contributor scoring from backend
+  // 3, devcare team contributor review
+  // 4, application detail from fillout (reponse aux question de la quest)
+
+  return <div>QuestApplicationPage</div>;
+}
