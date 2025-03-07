c631b5703dc47ae358c5c441650e9f9138ad7aea
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-03-07 14:40:30 +0100

    Remove feature flag for Discover page V2

--- app/(saas)/discover/page.tsx
+++ app/(saas)/discover/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import DiscoverPageV1 from "./v1";
 import DiscoverPageV2 from "./v2";
 
 export default function DiscoverPage() {
-  if (process.env.NEXT_PUBLIC_DISCOVER_V2_ENABLED === "true") {
-    return <DiscoverPageV2 />;
-  }
-
-  return <DiscoverPageV1 />;
+  return <DiscoverPageV2 />;
 }
