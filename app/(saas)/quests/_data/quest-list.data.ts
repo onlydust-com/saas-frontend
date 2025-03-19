45252646af2d36768d95343b02776921b19e912b
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-03-19 17:08:02 +0100

    clean quest

--- app/(saas)/quests/_data/quest-list.data.ts
+++ app/(saas)/quests/_data/quest-list.data.ts
@@ -218,8 +218,8 @@ export const QuestListData: QuestList[] = [
   },
 
   {
-    id: "d4b5d8e1-c2a3-4f6b-9d8e-1c2a3f6b9d82", // ??
-    bannerUrl: "https://pbs.twimg.com/profile_banners/1658760144077877250/1690476363/1500x500", // ??
+    id: "f8c47e9d-b3a5-4f16-8d92-3c7a1b94e5d0",
+    bannerUrl: "https://pbs.twimg.com/profile_banners/1658760144077877250/1690476363/1500x500",
     projectId: "ed69beff-183c-4df2-afa3-22d4b4e6b44a",
     projectSlug: "lyricflip",
     name: "Mobile app foundational pages",
@@ -229,17 +229,8 @@ export const QuestListData: QuestList[] = [
       title: "Mobile app foundational pages",
       description:
         "We have a couple of screens that goes midway into the screens we have on the mobile app version and we need the built rapidly by a group of 2 or 3 people. We want this build by a group because we want uniformity of appearance all over the app.",
-      requirements: [
-        "Only submit a pull request if assigned to this issue.",
-        "In your PR description, include 'Close #[issue_id]' to close the issue.",
-        "After submitting your PR, label it as 'ready for review'. Address any requested changes promptly and update the label back to 'ready for review' once done.",
-        "Test your PR locally before pushing and ensure all tests and builds pass.",
-      ], // ??,
-      warning:
-        "⚠️ Failure to follow these requirements may result in being added to the OnlyDust blacklist, affecting your eligibility for future rewards.", // ??
-      links: [
-        "https://www.figma.com/design/fh0OAvj4AS08kHoSxu3DkE/%F0%9F%9A%80-Starknet-Quest?node-id=6837-7063&t=Ph0cLxeucMJgr0Q9-1",
-      ], // ??,
+      warning: "",
+      links: [],
     },
     wantedProfiles: {
       junior: {
@@ -256,8 +247,8 @@ export const QuestListData: QuestList[] = [
       },
     },
     requiredSkills: ["react native", "expo", "UI", "typescript"],
-    startDate: "2025-03-24",
-    endDate: "2025-04-02",
+    startDate: "2025-03-20",
+    endDate: "2025-04-03",
     status: "application-open",
     issues: [
       2926956496, 2926953702, 2926950585, 2926948727, 2926944633, 2926943128, 2926941803, 2926940839, 2926939730,
