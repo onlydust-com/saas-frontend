f06cdfce5559e3e5975281946a3fb7bd050759d3
Alexis Benoliel <alexbenoliel@gmail.com>
2025-03-19 11:10:10 +0100

    Update quest-list.data.ts

--- app/(saas)/quests/_data/quest-list.data.ts
+++ app/(saas)/quests/_data/quest-list.data.ts
@@ -216,6 +216,55 @@ export const QuestListData: QuestList[] = [
     issues: [2866995946, 2866997140, 2866997973],
     maintainers: [19536159],
   },
+
+  {
+    id: "d4b5d8e1-c2a3-4f6b-9d8e-1c2a3f6b9d82", // ??
+    bannerUrl: "https://pbs.twimg.com/profile_banners/1658760144077877250/1690476363/1500x500", // ??
+    projectId: "ed69beff-183c-4df2-afa3-22d4b4e6b44a",
+    projectSlug: "lyricflip",
+    name: "Mobile app foundational pages",
+    shortDescription:
+      "We have a couple of screens that goes midway into the screens we have on the mobile app version and we need the built rapidly by a group of 2 or 3 people. We want this build by a group because we want uniformity of appearance all over the app.",
+    longDescription: {
+      title: "Mobile app foundational pages",
+      description:
+        "We have a couple of screens that goes midway into the screens we have on the mobile app version and we need the built rapidly by a group of 2 or 3 people. We want this build by a group because we want uniformity of appearance all over the app.",
+      requirements: [
+        "Only submit a pull request if assigned to this issue.",
+        "In your PR description, include 'Close #[issue_id]' to close the issue.",
+        "After submitting your PR, label it as 'ready for review'. Address any requested changes promptly and update the label back to 'ready for review' once done.",
+        "Test your PR locally before pushing and ensure all tests and builds pass.",
+      ], // ??,
+      warning:
+        "⚠️ Failure to follow these requirements may result in being added to the OnlyDust blacklist, affecting your eligibility for future rewards.", // ??
+      links: [
+        "https://www.figma.com/design/fh0OAvj4AS08kHoSxu3DkE/%F0%9F%9A%80-Starknet-Quest?node-id=6837-7063&t=Ph0cLxeucMJgr0Q9-1",
+      ], // ??,
+    },
+    wantedProfiles: {
+      junior: {
+        provided: [],
+        wanted: 0,
+      },
+      senior: {
+        provided: [],
+        wanted: 2,
+      },
+      expert: {
+        provided: [],
+        wanted: 0,
+      },
+    },
+    requiredSkills: ["react native", "expo", "UI", "typescript"],
+    startDate: "2025-03-24",
+    endDate: "2025-04-02",
+    status: "application-open",
+    issues: [
+      2926956496, 2926953702, 2926950585, 2926948727, 2926944633, 2926943128, 2926941803, 2926940839, 2926939730,
+      2926937458, 2926933443, 2926929902, 2926925029, 2926921661,
+    ],
+    maintainers: [51526246],
+  },
 ];
 
 // https://api.github.com/repos/Trustless-Work/Trustless-Work-Smart-Escrow/issues/41
