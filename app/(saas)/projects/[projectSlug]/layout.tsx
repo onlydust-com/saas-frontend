7056d299fbf9f6330a86108d2a2205e7faa79fe9
Mehdi Hamri <143011364+pixelfact@users.noreply.github.com>
2025-03-25 10:23:41 +0100

    handle all metadata

--- app/(saas)/projects/[projectSlug]/layout.tsx
+++ app/(saas)/projects/[projectSlug]/layout.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import { ReactNode } from "react";
 
+import { sharedMetadata } from "@/app/shared-metadata";
+
 import { GithubPermissionsProvider } from "@/shared/features/github-permissions/github-permissions.context";
 import { PageContainer } from "@/shared/features/page/page-container/page-container";
 import { ContributionsSidepanel } from "@/shared/panels/contribution-sidepanel/contributions-sidepanel";
@@ -9,13 +11,25 @@ import { ContributorSidepanel } from "@/shared/panels/contributor-sidepanel/cont
 import { PageHeader } from "./_features/page-header/page-header";
 
 export async function generateMetadata({ params }: { params: { projectSlug: string } }): Promise<Metadata> {
-  const projectSlug = params.projectSlug;
+  try {
+    const projectData = await fetch(
+      `https://${process.env.NEXT_PUBLIC_ONLYDUST_API_BASEPATH}/api/v1/projects/slug/${params.projectSlug}`
+    ).then(res => res.json());
 
-  return {
-    openGraph: {
-      images: [`https://previous.onlydust.com/p/${projectSlug}/opengraph-image`],
-    },
-  };
+    return {
+      ...sharedMetadata,
+      title: `${projectData.name} - OnlyDust`,
+      openGraph: {
+        ...sharedMetadata.openGraph,
+        title: `${projectData.name} - OnlyDust`,
+      },
+      twitter: {
+        ...sharedMetadata.twitter,
+      },
+    };
+  } catch {
+    return sharedMetadata;
+  }
 }
 
 export default function ProjectsLayout({ params, children }: { params: { projectSlug: string }; children: ReactNode }) {
