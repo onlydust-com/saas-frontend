7a83b38976f07e1b8023a8997f2fab187014e602
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-03-25 16:09:41 +0100

    refactor: enhance kanban layout with dynamic column handling

--- shared/features/kanban/kanban.tsx
+++ shared/features/kanban/kanban.tsx
@@ -1,11 +1,18 @@
+import { cn } from "@/shared/utils";
+
 import { KanbanProps } from "./kanban.types";
 
 export function Kanban({ children: _children }: KanbanProps) {
   const children = Array.isArray(_children) ? _children : [_children];
 
   return (
-    <div className={"h-full w-full overflow-auto"}>
-      <div className={`grid h-full min-h-[500px] min-w-[1600px] grid-cols-${children.length} gap-lg`}>
+    <div className={"h-full w-full overflow-x-auto"}>
+      <div
+        className={cn(
+          "grid h-full min-h-[500px] min-w-[1600px] gap-lg",
+          children.length === 5 ? "grid-cols-5" : "grid-cols-4"
+        )}
+      >
         {children.map(child => child)}
       </div>
     </div>
