8d047eb06795820876d0d9c5c79e680e22ba3fff
PierreOucif <pierre.oucif@gadz.org>
2025-04-03 13:21:57 +0200

    Order quests by statuses

--- app/(saas)/quests/_features/quest-list/quest-list.tsx
+++ app/(saas)/quests/_features/quest-list/quest-list.tsx
@@ -6,11 +6,25 @@ import { NEXT_ROUTER } from "@/shared/constants/router";
 
 import { QuestCard } from "../../_components/quest-card/quest-card";
 
+const statusPriority = {
+  started: 0,
+  "application-open": 1,
+  finished: 2,
+};
+
 export function QuestList() {
   const router = useRouter();
-  const quests = QuestListData;
+  
+  // Sort quests by status priority
+  const sortedQuests = [...QuestListData].sort((a, b) => {
+    return statusPriority[a.status] - statusPriority[b.status];
+  });
 
-  return quests.map(quest => (
-    <QuestCard key={quest.id} {...quest} onClick={() => router.push(NEXT_ROUTER.quests.details.root(quest.id))} />
-  ));
+  return (
+    <div className="flex flex-col gap-4">
+      {sortedQuests.map(quest => (
+        <QuestCard key={quest.id} {...quest} onClick={() => router.push(NEXT_ROUTER.quests.details.root(quest.id))} />
+      ))}
+    </div>
+  );
 }
