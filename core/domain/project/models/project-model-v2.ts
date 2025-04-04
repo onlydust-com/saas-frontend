9564a8c4be1cbf4f2d8f5351c28bd35daeee2c1e
haydencleary <5160414+haydencleary@users.noreply.github.com>
2025-04-04 14:18:32 +0200

    refactor: update project model to use ProjectOrRepoResponseV2 schema

--- core/domain/project/models/project-model-v2.ts
+++ core/domain/project/models/project-model-v2.ts
@@ -2,36 +2,40 @@ import { components } from "@/core/infrastructure/marketplace-api-client-adapter
 
 import { ProjectCategory, ProjectCategoryInterface } from "../../project-category/models/project-category-model";
 
-type ProjectResponseV2 = components["schemas"]["ProjectResponseV2"];
+type ProjectOrRepoResponseV2 = components["schemas"]["ProjectOrRepoResponseV2"];
 
-export interface ProjectInterfaceV2 extends ProjectResponseV2 {
+export interface ProjectInterfaceV2 extends ProjectOrRepoResponseV2 {
   categories: ProjectCategoryInterface[];
 }
 
 export class ProjectV2 implements ProjectInterfaceV2 {
-  id!: ProjectResponseV2["id"];
-  slug!: ProjectResponseV2["slug"];
-  name!: ProjectResponseV2["name"];
-  logoUrl!: ProjectResponseV2["logoUrl"];
-  shortDescription!: ProjectResponseV2["shortDescription"];
-  longDescription!: ProjectResponseV2["longDescription"];
-  contributorCount!: ProjectResponseV2["contributorCount"];
-  starCount!: ProjectResponseV2["starCount"];
-  forkCount!: ProjectResponseV2["forkCount"];
-  availableIssueCount!: ProjectResponseV2["availableIssueCount"];
-  goodFirstIssueCount!: ProjectResponseV2["goodFirstIssueCount"];
+  id!: ProjectOrRepoResponseV2["id"];
+  slug!: ProjectOrRepoResponseV2["slug"];
+  name!: ProjectOrRepoResponseV2["name"];
+  logoUrl!: ProjectOrRepoResponseV2["logoUrl"];
+  shortDescription!: ProjectOrRepoResponseV2["shortDescription"];
+  longDescription!: ProjectOrRepoResponseV2["longDescription"];
+  contributorCount!: ProjectOrRepoResponseV2["contributorCount"];
+  starCount!: ProjectOrRepoResponseV2["starCount"];
+  forkCount!: ProjectOrRepoResponseV2["forkCount"];
+  availableIssueCount!: ProjectOrRepoResponseV2["availableIssueCount"];
+  goodFirstIssueCount!: ProjectOrRepoResponseV2["goodFirstIssueCount"];
   categories!: ProjectCategoryInterface[];
-  languages!: ProjectResponseV2["languages"];
-  ecosystems!: ProjectResponseV2["ecosystems"];
-  leads!: ProjectResponseV2["leads"];
-  moreInfos!: ProjectResponseV2["moreInfos"];
-  mergedPrCount!: ProjectResponseV2["mergedPrCount"];
-  currentWeekAvailableIssueCount!: ProjectResponseV2["currentWeekAvailableIssueCount"];
-  currentWeekMergedPrCount!: ProjectResponseV2["currentWeekMergedPrCount"];
-  tags!: ProjectResponseV2["tags"];
-  repos!: ProjectResponseV2["repos"];
+  languages!: ProjectOrRepoResponseV2["languages"];
+  ecosystems!: ProjectOrRepoResponseV2["ecosystems"];
+  leads!: ProjectOrRepoResponseV2["leads"];
+  moreInfos!: ProjectOrRepoResponseV2["moreInfos"];
+  mergedPrCount!: ProjectOrRepoResponseV2["mergedPrCount"];
+  currentWeekAvailableIssueCount!: ProjectOrRepoResponseV2["currentWeekAvailableIssueCount"];
+  currentWeekMergedPrCount!: ProjectOrRepoResponseV2["currentWeekMergedPrCount"];
+  tags!: ProjectOrRepoResponseV2["tags"];
+  repos!: ProjectOrRepoResponseV2["repos"];
+  odHackStats!: ProjectOrRepoResponseV2["odHackStats"];
+  contributorStats!: ProjectOrRepoResponseV2["contributorStats"];
+  isProject!: ProjectOrRepoResponseV2["isProject"];
+  overview!: ProjectOrRepoResponseV2["overview"];
 
-  constructor(props: ProjectResponseV2) {
+  constructor(props: ProjectOrRepoResponseV2) {
     Object.assign(this, props);
     this.categories = (props.categories || []).map(category => new ProjectCategory(category));
   }
